7ebd35e7c72612b96a43f839d31009e633fbc457
Jonnie Stuefer <jonnie@floorspot.org>
2023-07-14 11:10:25 +0200

    Adding new landing page

---
+++ sonicom/resources/views/landing.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            SONICOM Ecosystem
+        </h2>
+    </x-slot>
+    <p>The SONICOM Ecosystem bla bla bla</p>
+</x-app-layout>
