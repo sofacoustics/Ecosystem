58bc448242d97658da75a47e35eae11c88eab941
Jonnie Stuefer <jonnie@floorspot.org>
2023-07-14 11:17:55 +0200

    using x-layout

--- sonicom/resources/views/datasets.blade.php
+++ sonicom/resources/views/datasets.blade.php
@@ -1,11 +1,20 @@
-<h1> Datasets </h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Datasets
+        </h2>
+    </x-slot>
+    <p>The following datasets are available</p>
+    <ul>
+    @if (isset($datasets->data))
+        @foreach ($datasets->data as $dataset)
+        <li> {{ $dataset->descriptiveMetadata->title }} </li>
+        @endforeach
+    @else
+        <p>There are not datasets in this workspace!</p>    
+    @endif
+    </ul>
+</x-app-layout>
 
-<ul>
-@if (isset($datasets->data))
-    @foreach ($datasets->data as $dataset)
-    <li> {{ $dataset->descriptiveMetadata->title }} </li>
-    @endforeach
-@else
-    <p>There are not datasets in this workspace!</p>    
-@endif
-</ul>
+
+
