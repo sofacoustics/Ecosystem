539d04fac3d443000c3bc99acf241668423b23bd
Jonnie Stuefer <jonnie@floorspot.org>
2023-07-25 07:35:35 +0200

    Bugfix: landing page now working even if not logged in

--- sonicom/resources/views/layouts/navigation.blade.php
+++ sonicom/resources/views/layouts/navigation.blade.php
@@ -18,7 +18,8 @@
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
+						<!-- Settings Dropdown -->
+						@auth						
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -129,7 +130,8 @@
             </a>
         @endforeach
       </div>
-        <!-- Settings Dropdown -->
+				<!-- Settings Dropdown -->
+			  @auth
         <div class="hidden sm:flex sm:items-center sm:ml-6">
             <x-dropdown align="right" width="48">
                 <x-slot name="trigger">
@@ -161,6 +163,9 @@
                     </form>
                 </x-slot>
             </x-dropdown>
-        </div>
+				</div>
+				@else
+						<a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+			  @endauth
     </div>
   </nav>
