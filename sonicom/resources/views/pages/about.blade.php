cb735ef5feab17f5aafab5391dc80b55e0ef14fb
Jonnie Stuefer <jonnie@floorspot.org>
2023-07-28 13:43:33 +0200

    Current

--- sonicom/resources/views/pages/about.blade.php
+++ sonicom/resources/views/pages/about.blade.php
@@ -5,5 +5,6 @@
         </h2>
     </x-slot>
 		<p>SONICOM: Transforming Auditory-Based Social Interaction and Communication in AR/VR</p>
+		<p>Immersive audio is our everyday experience of being able to hear and interact with sounds around us. Simulating spatially located sounds in virtual or augmented reality (VR/AR) must be done in a unique way for each individual and currently requires expensive, time-consuming individual measurements, making it commercially unfeasible. The impact of immersive audio beyond perceptual metrics such as presence and localisation is still an unexplored area of research, specifically when related with social interaction, entering the behavioural and cognitive realms.</p>
 	  <p>Put some more stuff here</p>
 </x-app-layout>
