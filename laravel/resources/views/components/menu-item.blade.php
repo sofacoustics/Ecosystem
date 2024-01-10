c900ec516fd44949f7ac763a842aec364c1fe025
Jonnie Stuefer <jonathan.stuefer@oeaw.ac.at>
2024-01-10 15:03:18 +0100

    Submenus now using 'route' column to identify parent menu item

--- laravel/resources/views/components/menu-item.blade.php
+++ laravel/resources/views/components/menu-item.blade.php
@@ -1,12 +1,24 @@
 {{--https://stackoverflow.com/questions/38665770/recursive-display-of-data-with-blade-laravel --}}
 {{-- https://coding-lesson.com/creating-dynamic-nested-menus-in-laravel-with-relationships --}}
-<li>
-    <a href="{{ $menuItem->url }}">{{ $menuItem->title }}</a>
+<li class="mr-1">
+    <a class="inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold" href="{{ $menuItem->url() }}">{{ $menuItem->title }}</a>
     @if($menuItem->children->count())
-        <ul>
-            @foreach($menuItem->children as $child)
-                <x-menu-item :menuItem="$child"/>
-            @endforeach
-        </ul>
+        @if (\Request::routeIs($menuItem->route) == false)
+          {{-- These submenus belong to the wrong route --}}
+        @elseif (!Auth::check() && $subMenuItem->authenticated == 1)
+          {{-- This item is restricted to authenticated users --}}
+          c
+        @else
+          @if (!auth()->user()->hasRole('admin') && $subMenuItem->authenticated == 2)
+            {{-- This item is restricted to users with the 'admin' role --}}
+            d
+          @else
+            <ul>
+                @foreach($menuItem->children as $child)
+                    <x-menu-item :menuItem="$child"/>
+                @endforeach
+            </ul>
+          @endif
+        @endif
     @endif
 </li>
