11578509fd3a14b48e57cba80d393e60ba8d5642
Jonnie Stuefer <jonathan.stuefer@oeaw.ac.at>
2024-01-10 15:02:27 +0100

    Added submenu to index page

--- laravel/resources/views/admin/index.blade.php
+++ laravel/resources/views/admin/index.blade.php
@@ -10,5 +10,13 @@
             <strong>{{ $message }}</strong>
         </div>
     @endif
-    jw:todo
+    {{-- sub-menus --}}
+    {{-- https://coding-lesson.com/creating-dynamic-nested-menus-in-laravel-with-relationships/ --}}
+    <ul class="flex border-b">
+        @foreach(\App\Models\MenuItem::where('parent_id', \App\Models\MenuItem::where('route', 'admin')->first()->id)->get() as $menuItem)
+            <x-menu-item :menuItem="$menuItem"/>
+        @endforeach
+    </ul>
+
+    jw:todo the admin.index view
 </x-app-layout>
