65dd0a1b159c2230e00ed9c6e752c87e44f012fc
Jonnie Stuefer <jonnie@floorspot.org>
2024-01-18 12:02:08 +0100

    Adding 'Datasetdef' model, migration, seeder, etc.

---
+++ laravel/app/Http/Controllers/DatasetdefController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDatasetdefRequest;
+use App\Http\Requests\UpdateDatasetdefRequest;
+use App\Models\Datasetdef;
+
+class DatasetdefController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDatasetdefRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Datasetdef $datasetdef)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Datasetdef $datasetdef)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDatasetdefRequest $request, Datasetdef $datasetdef)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Datasetdef $datasetdef)
+    {
+        //
+    }
+}
