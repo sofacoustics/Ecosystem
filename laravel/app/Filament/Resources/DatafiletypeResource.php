ca79d56aa15a1f591ed7c343f78cd9824a62e25b
Jonnie Stuefer <jonnie@floorspot.org>
2024-01-19 10:05:56 +0100

    Removing 'Datafiletype' Filament relationship

--- laravel/app/Filament/Resources/DatafiletypeResource.php
+++ laravel/app/Filament/Resources/DatafiletypeResource.php
@@ -64,7 +64,6 @@ class DatafiletypeResource extends Resource
     public static function getRelations(): array
     {
         return [
-					RelationManagers\DatafilesRelationManager::class,
         ];
     }
 
