3cfe19d2be2cd4060991ccae19533377be0b450d
Jonnie Stuefer <jonnie@floorspot.org>
2024-02-26 13:10:43 +0100

    Adding ARI B datasetdef

--- laravel/database/seeders/DatasetdefSeeder.php
+++ laravel/database/seeders/DatasetdefSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Datasetdef;
+
 class DatasetdefSeeder extends Seeder
 {
     /**
@@ -13,5 +15,9 @@ class DatasetdefSeeder extends Seeder
     public function run(): void
     {
         //
+        Datasetdef::create([ 'database_id' => 1, 'name' => 'dtf b', 'datafiletype_id' => 3 ]);
+        Datasetdef::create([ 'database_id' => 1, 'name' => 'hrtf b', 'datafiletype_id' => 3 ]);
+        Datasetdef::create([ 'database_id' => 1, 'name' => 'ear left', 'datafiletype_id' => 1 ]);
+        Datasetdef::create([ 'database_id' => 1, 'name' => 'ear right', 'datafiletype_id' => 1 ]);
     }
 }
