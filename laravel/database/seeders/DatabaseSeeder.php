558f1be14e555a4f68a58f5df0d28af7c79494ea
Jonnie Stuefer <jonnie@floorspot.org>
2024-02-26 13:03:08 +0100

    Removing Datafiletype_Datasetdef pivot table (using just Datasetdef table instead)

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -22,15 +22,15 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call(RolesAndPermissionsSeeder::class);
         $this->call(UserSeeder::class);
-        Database::create(array('name' => 'ARI (LAS)', 'description' => 'n-the-ear HRTFs and DTFs are measured in the ARI loudspeaker array studio (LAS). Each set of HRTFs contains 451 sound-source directions (91 loudspeakers by 5 subject roations), see McLachlan et al. (2023)', 'user_id' => 1));
-        Database::create(array('name' => 'CHEDAR', 'description' => 'Numerically calculated HRTFs (.sofa) with 3D meshes of the head and pinnae (.ply) and anthropometric data (.mat) provided. For more details, see the documentation. (Credit: Slim Ghorbal, France)', 'user_id' => 1));
-        Database::create(array('name' => 'Jonnie\'s ARI SOFA test 2', 'description' => 'Nr. 2. \'Nuf said!', 'user_id' => 1, 'radar_id' => 'dEZxRRrxpiHSzbBZ'));
-				$this->call([
-					DatasetSeeder::class,
-					DatafiletypeSeeder::class,
-					DatafileSeeder::class,
-					MenuItemSeeder::class,
-				]);
+        Database::create(array('name' => 'ARI B', 'description' => 'Specialty: hrtfs, dtfs, and photos of the ears', 'user_id' => 1));
+        //Database::create(array('name' => 'Jonnie\'s ARI SOFA test 2', 'description' => 'Nr. 2. \'Nuf said!', 'user_id' => 1, 'radar_id' => 'dEZxRRrxpiHSzbBZ'));
+        $this->call([
+            DatasetSeeder::class,
+            DatafiletypeSeeder::class,
+            DatasetdefSeeder::class,
+            DatafileSeeder::class,
+            MenuItemSeeder::class,
+        ]);
 
     }
 }
