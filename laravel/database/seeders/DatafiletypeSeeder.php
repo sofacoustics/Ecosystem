eb3ad82c81ceaf3c9d0a3dd1e114c324b7f0ee44
Jonnie Stuefer <jonnie@floorspot.org>
2024-02-26 13:09:57 +0100

    Removing unnecessary datafiletypes

--- laravel/database/seeders/DatafiletypeSeeder.php
+++ laravel/database/seeders/DatafiletypeSeeder.php
@@ -17,7 +17,5 @@ class DatafiletypeSeeder extends Seeder
        Datafiletype::create([ 'name' => 'jpg', 'description' => 'A jpg image file', ]);
        Datafiletype::create([ 'name' => 'png', 'description' => 'A png image file', ]);
        Datafiletype::create([ 'name' => 'HRTF.sofa', 'description' => 'A HRTF file in SOFA format', ]);
-       Datafiletype::create([ 'name' => 'BRIR.sofa', 'description' => 'A BRIR file in SOFA format', ]);
-       Datafiletype::create([ 'name' => 'DTF.sofa', 'description' => 'A DTF file in SOFA format', ]);
     }
 }
