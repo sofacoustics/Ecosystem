253483348d2923c8fff2abe4b6407029ace9925e
Jonnie Stuefer <jonnie@floorspot.org>
2024-02-29 11:27:47 +0100

    Adding 'path' to datafile table

--- laravel/database/migrations/2023_09_05_084636_create_datafiles_table.php
+++ laravel/database/migrations/2023_09_05_084636_create_datafiles_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('dataset_id')->unsigned();
             $table->integer('datafiletype_id')->unsigned();
+            $table->string('path');
             $table->foreign('dataset_id')->references('id')->on('datasets')->onDelete('cascade');
             $table->foreign('datafiletype_id')->references('id')->on('datafiletypes')->onDelete('cascade');
             $table->timestamps();
