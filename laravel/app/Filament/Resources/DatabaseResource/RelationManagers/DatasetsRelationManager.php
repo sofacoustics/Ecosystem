eebafdba8060cc5d6d4d421aa48fd381d36181d2
Jonnie Stuefer <jonnie@floorspot.org>
2024-03-04 12:26:49 +0100

    Make child relationship viewable via 'actions'

--- laravel/app/Filament/Resources/DatabaseResource/RelationManagers/DatasetsRelationManager.php
+++ laravel/app/Filament/Resources/DatabaseResource/RelationManagers/DatasetsRelationManager.php
@@ -40,6 +40,7 @@ class DatasetsRelationManager extends RelationManager
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
