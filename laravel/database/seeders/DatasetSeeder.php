39562fca30bdb5943104124a369ad197d9cb3253
Jonnie Stuefer <jonnie@floorspot.org>
2024-03-07 11:05:47 +0100

    Updating seeder

--- laravel/database/seeders/DatasetSeeder.php
+++ laravel/database/seeders/DatasetSeeder.php
@@ -13,8 +13,8 @@ class DatasetSeeder extends Seeder
      */
     public function run(): void
     {
-        Dataset::create(array('name' => 'nh4', 'description' => 'nh4', 'database_id' => 1));
-        Dataset::create(array('name' => 'nh5', 'description' => 'nh5', 'database_id' => 1));
-        Dataset::create(array('name' => 'nh4', 'description' => 'nh4', 'database_id' => 2));
+        Dataset::create(array('name' => 'nh4', 'description' => 'normal hearer 4', 'database_id' => 1));
+        Dataset::create(array('name' => 'nh5', 'description' => 'normal hearer 5', 'database_id' => 1));
+        Dataset::create(array('name' => 'nh4', 'description' => 'normal hearer 4', 'database_id' => 2));
     }
 }
