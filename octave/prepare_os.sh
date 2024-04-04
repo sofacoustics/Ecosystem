55f1ff946df4dee2b9a755555b911fd2d82b904f
Jonnie Stuefer <jonnie@floorspot.org>
2024-04-04 11:21:09 +0200

    Adding necessary packages

--- octave/prepare_os.sh
+++ octave/prepare_os.sh
@@ -3,5 +3,5 @@
 #	Prepare OS for Octave/SOFA use
 #
 #apt install octave-netcdf netcdf-bin liboctave-dev libnetcdf-dev
-apt install octave-netcdf
+apt install octave-netcdf liboctave-dev libnetcdf-dev
 
