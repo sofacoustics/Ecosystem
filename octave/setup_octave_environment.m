59988c6d3fad2b1326425e9afa5eebfa923bdb31
Jonnie Stuefer <jonnie@floorspot.org>
2024-04-04 11:21:53 +0200

    Improving path code

--- octave/setup_octave_environment.m
+++ octave/setup_octave_environment.m
@@ -2,10 +2,15 @@
 %
 % Note that installing netcdf didn't work when running the snap ocatve package
 
-sofapath = './SOFAtoolbox';
+file_path = fileparts(mfilename('fullpath'));
 
+disp (["Adding script directory " file_path " to path"]);
+addpath(file_path);
+
+sofapath = [file_path '/SOFAtoolbox'];
 disp (["Adding sofapath " sofapath " to path"]);
 addpath(sofapath);
+
 path();
 
 disp ("Installing netcdf");
