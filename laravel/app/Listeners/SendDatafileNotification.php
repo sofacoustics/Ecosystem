a605892c2c1b7e4f035335bf0874f7162f8183da
Jonnie Stuefer <jonnie@floorspot.org>
2024-04-09 13:01:31 +0200

    Using correct path for public files

--- laravel/app/Listeners/SendDatafileNotification.php
+++ laravel/app/Listeners/SendDatafileNotification.php
@@ -26,6 +26,8 @@ class SendDatafileNotification implements ShouldQueue
     public function handle(DatafileUploaded $event): void
     {
       //jw:note You can only really debug a listener if the QUEUE_CONNECTION is 'sync'
+      $name = $event->datafile->name;
+      app('log')->info('SendDatafileNotification Listener');
       HRTFCreateFigures::dispatch($event->datafile);
       //HRTFCreateFigures::dispatchSync($event->datafile); //jw:note this didn't help. Only QUEUE_CONNECTION helped.
     }
--- laravel/app/Observers/DatafileObserver.php
+++ laravel/app/Observers/DatafileObserver.php
@@ -2,8 +2,11 @@
 
 namespace App\Observers;
 
+use App\Jobs\HRTFCreateFigures;
+use App\Jobs\OctavePipe;
 use App\Models\Datafile;
 use App\Events\DatafileUploaded;
+use Illuminate\Support\Facades\Storage;
 
 class DatafileObserver
 {
@@ -21,9 +24,39 @@ class DatafileObserver
     public function updated(Datafile $datafile): void
     {
         //
+        app('log')->info('DatafileObserver::updated()');
         $path = $datafile->path();
+        $name = $datafile->name;
         $location = $datafile->location;
-        DatafileUploaded::dispatch($datafile);
+        $localpath = $datafile->localpath();
+        //HRTFCreateFigures::dispatch($datafile);
+        // if location exists, then send to Octave job
+        if($datafile->location != null) 
+        {
+            //jw:tmp:start
+            $dataset = $datafile->dataset->name;
+            app('log')->info('DatafileObserver::updated() - dataset->name = ' . $dataset);
+            $database = $datafile->dataset->database->name;
+            app('log')->info('DatafileObserver::updated() - database->name = ' . $database);
+            $datasetdef = $datafile->datasetdef;
+            app('log')->info('DatafileObserver::updated() - datasetdef = ' . $datasetdef);
+            $tool = $datasetdef->tool;
+            if($tool == null)
+            {
+                app('log')->info('DatafileObserver::updated() - no tool defined');
+                return;
+            }
+            else
+                app('log')->info('DatafileObserver::updated() - tool = ' . $tool);
+            $storage_path = storage_path();
+            $storage_disk_path = Storage::disk('public')->path($datafile->location);
+            $path = Storage::path($datafile->location);
+            app('log')->info('DatafileObserver::updated() - $tool->functionname = ' . $tool->functionname); 
+            //jw:note If you want to debug a job, you *must* use the 'sync' queue, not the 'database' queue
+            app('log')->info('DatafileObserver::updated() - name = ' . $datafile->name . ' location = ' . $datafile->location . ' path = ' . $datafile->path() . ' storage_path = ' . $storage_path . ' storage_disk_path = ' . $storage_disk_path);
+            OctavePipe::dispatch("/tmp/sonicom-octave-pipe", "$tool->functionname $storage_disk_path");
+        }
+        //DatafileUploaded::dispatch($datafile);
     }
 
     /**
