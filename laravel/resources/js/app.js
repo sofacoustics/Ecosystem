79ddf1ac80b5b70e783af948b5a14f0cb4687ca9
Jonnie Stuefer <jonnie@floorspot.org>
2024-05-14 12:12:42 +0200

    Removing Alpine from app.js, since livewire includes it

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+//jw:note removing these three lines, since 'livewire' includes alpine (https://livewire.laravel.com/docs/troubleshooting#removing-laravel-breezes-alpine)
+//import Alpine from 'alpinejs';
+//window.Alpine = Alpine;
+//Alpine.start();
