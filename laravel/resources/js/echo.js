a8710448f27a736a082c10f11071888a82fb5769
Jonnie Stuefer <jonnie@floorspot.org>
2024-05-14 14:11:07 +0200

    Listening on 'sonicom-ecosystem' channel

--- laravel/resources/js/echo.js
+++ laravel/resources/js/echo.js
@@ -12,3 +12,18 @@ window.Echo = new Echo({
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
+
+//jw:note This *is* necessary!
+window.Echo.channel("sonicom-ecosystem")
+    .listen(".test-event", (event) => {
+	    console.log("sonicom-ecosystem: message = " + event.message);
+	    //console.log(event);
+        //alert(event?.message)
+});
+
+/*
+window.Echo.channel("test").listen(".test-event", (event) => {
+	    console.log("test: message = " + event.message);
+	    //console.log(event);
+        //alert(event?.message)
+});*/
