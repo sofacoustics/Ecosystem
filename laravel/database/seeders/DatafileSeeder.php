ccfda1fce75d9906884d25f2d23ebb4192b5704b
Jonnie Stuefer <jonnie@floorspot.org>
2024-06-11 12:50:08 +0200

    Bugfix: correcting some file names

--- laravel/database/seeders/DatafileSeeder.php
+++ laravel/database/seeders/DatafileSeeder.php
@@ -23,7 +23,7 @@ class DatafileSeeder extends Seeder
         Datafile::create(array('name' => 'ear left.JPG', 'location' => NULL, 'dataset_id' => 2, 'datasetdef_id' => 3 ));
         Datafile::create(array('name' => 'ear right.JPG', 'location' => NULL, 'dataset_id' => 2, 'datasetdef_id' => 4 ));
         Datafile::create(array('name' => 'BezierPPM.csv', 'location' => NULL, 'dataset_id' => 3, 'datasetdef_id' => 5 ));
-        Datafile::create(array('name' => 'dtf b_nh4', 'location' => NULL, 'dataset_id' => 3, 'datasetdef_id' => 6 ));
-        Datafile::create(array('name' => 'hrtf b_nh4', 'location' => NULL, 'dataset_id' => 3, 'datasetdef_id' => 7 ));
+        Datafile::create(array('name' => 'dtf b_nh4.sofa', 'location' => NULL, 'dataset_id' => 3, 'datasetdef_id' => 6 ));
+        Datafile::create(array('name' => 'hrtf b_nh4.sofa', 'location' => NULL, 'dataset_id' => 3, 'datasetdef_id' => 7 ));
     }
 }
