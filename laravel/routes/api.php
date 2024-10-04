8ab0e7d0b8cd80ac54a949b5b48a11606b36ec2f
Jonnie Stuefer <jonnie@floorspot.org>
2024-10-04 12:19:12 +0200

    Added API DatabaseResource

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Resources\DatabaseResource;
+use App\Models\Database;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('databases/{id}', function (string $id) {
+    return new DatabaseResource(Database::findOrFail($id));
 });
