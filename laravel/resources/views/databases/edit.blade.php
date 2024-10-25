8a3946a6019c1e2a1edcd94a686d5eaadb761107
Jonnie Stuefer <jonnie@floorspot.org>
2024-10-25 13:20:56 +0200

    Bugfix: now using descriptiveMetadata

--- laravel/resources/views/databases/edit.blade.php
+++ laravel/resources/views/databases/edit.blade.php
@@ -32,26 +32,26 @@
             <legend>RADAR Dataset</legend>
         {{-- 'title' will be used by RadardatasetpureData as well --}}
         <label for="productionYear">Production Year
-            <input type="text" name="productionYear" id="productionYear" value="{{ $database->radardataset?->productionYear }}">
+            <input type="text" name="productionYear" id="productionYear" value="{{ $database->radardataset?->descriptiveMetadata->productionYear }}">
         </label><br/>
         <label for="resource">Resource Type
             {{-- use resource[value] *not* resource['value'] --}}
-            <input type="text" name="resource[value]" id="resourcevalue" value="{{ $database->radardataset?->resource->value }}">
-            <input type="text" name="resource[resourceType] id="resourcetype" value="{{ $database->radardataset?->resource->resourceType }}">
+            <input type="text" name="resource[value]" id="resourcevalue" value="{{ $database->radardataset?->descriptiveMetadata->resource->value }}">
+            <input type="text" name="resource[resourceType] id="resourcetype" value="{{ $database->radardataset?->descriptiveMetadata->resource->resourceType }}">
         </label><br/>
         <label>Rights
-            <input type="text" name="rights[controlledRights]" value" value="{{ $database->radardataset?->rights->controlledRights}}">
-            <input type="text" name="rights[additionalRights] value="{{ $database->radardataset?->rights->additionalRights}}">
+            <input type="text" name="rights[controlledRights]" value" value="{{ $database->radardataset?->descriptiveMetadata->rights->controlledRights}}">
+            <input type="text" name="rights[additionalRights] value="{{ $database->radardataset?->descriptiveMetadata->rights->additionalRights}}">
         </label><br/>
         <label>Subject Areas
-            <input type="text" name="subjectAreas[subjectArea][0][controlledSubjectAreaName]" value="{{ $database->radardataset?->subjectAreas->subjectArea[0]['controlledSubjectAreaName'] }}">
-            <input type="text" name="subjectAreas[subjectArea][0][additionalSubjectAreaName]" value="{{ $database->radardataset?->subjectAreas->subjectArea[0]['additionalSubjectAreaName'] }}">
+            <input type="text" name="subjectAreas[subjectArea][0][controlledSubjectAreaName]" value="{{ $database->radardataset?->descriptiveMetadata->subjectAreas->subjectArea[0]['controlledSubjectAreaName'] }}">
+            <input type="text" name="subjectAreas[subjectArea][0][additionalSubjectAreaName]" value="{{ $database->radardataset?->descriptiveMetadata->subjectAreas->subjectArea[0]['additionalSubjectAreaName'] }}">
         </label><br/>
         <label>Publishers
-            <input type="text" name="publishers[publisher][0][value]" value="{{ $database->radardataset?->publishers->publisher[0]['value'] }}">
+            <input type="text" name="publishers[publisher][0][value]" value="{{ $database->radardataset?->descriptiveMetadata->publishers->publisher[0]['value'] }}">
         </label><br/>
         <label>Creators
-            <input type="text" name="creators[creator][0][creatorName]" value="{{ $database->radardataset?->creators->creator[0]['creatorName'] }}">
+            <input type="text" name="creators[creator][0][creatorName]" value="{{ $database->radardataset?->descriptiveMetadata->creators->creator[0]['creatorName'] }}">
         </label><br/>
         </fieldset>
         <input type="submit" name="submit" value="Submit">
