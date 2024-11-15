236ab6c09505d3691d252523bf805222b1338aa5
Jonnie Stuefer <jonnie@floorspot.org>
2024-11-15 16:02:48 +0100

    Adding empty Radar Dataset component

---
+++ laravel/app/Livewire/Radar/Dataset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Radar;
+
+use Livewire\Component;
+
+class Dataset extends Component
+{
+    public function render()
+    {
+        return view('livewire.radar.dataset');
+    }
+}
