c052f00d72008f59f9132f98972955c37ee969e6
Jonnie Stuefer <jonnie@floorspot.org>
2024-11-15 13:15:41 +0100

    Adding general button component using form

---
+++ laravel/resources/views/components/button.blade.php
@@ -0,0 +1,6 @@
+<!-- A general purpose button 
+<form $attributes method="$method" action="$action"> -->
+<form {{ $attributes }}>
+        <input class="rounded-full border bg-amber-200 p-2 m-2" type="submit" value="{{ $slot }}">
+</form>
+
