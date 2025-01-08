f7ce276649b26aeec20ccaff150d7ea4f2435d96
Jonnie Stuefer <jonnie@floorspot.org>
2025-01-08 10:19:33 +0100

    Bugfix: database name now showing (actually called 'title')

--- laravel/resources/views/datasets/show.blade.php
+++ laravel/resources/views/datasets/show.blade.php
@@ -3,7 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
         Dataset: {{ $dataset->name }}
         </h2>
-        Database: <a href="{{ route('databases.show', $dataset->database->id) }}">{{ $dataset->database->name }}</a>
+        Database: <a href="{{ route('databases.show', $dataset->database->id) }}">{{ $dataset->database->title }}</a>
     </x-slot>
     <p>This dataset contains {{ count($dataset->datafiles) }} datafiles</p>
     {{--
