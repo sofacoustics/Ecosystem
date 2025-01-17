a17392c663a0e1008599c32ba0ec6f4d3156297d
Jonnie Stuefer <jonnie@floorspot.org>
2025-01-17 09:20:31 +0100

    Cosmetic: reordering dataset/database

--- laravel/resources/views/datasets/show.blade.php
+++ laravel/resources/views/datasets/show.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        Dataset: {{ $dataset->name }}
-        </h2>
         Database: <a href="{{ route('databases.show', $dataset->database->id) }}">{{ $dataset->database->title }}</a>
+        </h2>
+        Dataset: {{ $dataset->name }}
     </x-slot>
     <p>This dataset contains {{ count($dataset->datafiles) }} datafiles</p>
     {{--
