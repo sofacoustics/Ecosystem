2d3bab5729427a65db2fa4fabfa79e3dd7c0da76
Jonnie Stuefer <jonnie@floorspot.org>
2025-01-17 09:19:42 +0100

    Displaying number of datasetdefs

--- laravel/resources/views/databases/datasetdefs/index.blade.php
+++ laravel/resources/views/databases/datasetdefs/index.blade.php
@@ -2,11 +2,13 @@
     <x-slot name="header">
         <x-database.header :database=$database />
     </x-slot>
+    <h3>Datasetdef</h3>
+    <p>The {{ $database->title }} dataset definition (datasetdef) contains {{ count($database->datasetdefs) }} files. Each dataset must consist of one exemplar of each of these files.</p>
 
+    <ul class="list-disc list-inside">
     @foreach($database->datasetdefs as $datasetdef)
-        <div>
-            <p>id: {{ $datasetdef->id }}, name: {{ $datasetdef->name }}, type: {{ $datasetdef->datafiletype->name }}</p>
-        </div>
+            <li>{{ $datasetdef->name }}, type: {{ $datasetdef->datafiletype->name }}</li>
     @endforeach
+    </ul>
 
 </x-app-layout>
