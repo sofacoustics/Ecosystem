79757ea96e69517a4f660dda80574b0c4866f041
Jonnie Stuefer <jonnie@floorspot.org>
2025-01-30 14:07:37 +0100

    Simplifying auth check for edit/add creator and now using x-button

--- laravel/resources/views/databases/creators/edit.blade.php
+++ laravel/resources/views/databases/creators/edit.blade.php
@@ -6,14 +6,7 @@
         <x-database.header :database="$creator->database" />
     </x-slot>
     <div>
-		<p>
-                @if($creator)
-                    <h3>Edit a creator:</h3>
-                @else
-										<h3>Add a new creator:</h3>
-                @endif
-            </p>
-		
+
 			<livewire:creator-form :database="$creator->database" :creator=$creator />
     </div>
 </x-app-layout>
--- laravel/resources/views/databases/creators/index.blade.php
+++ laravel/resources/views/databases/creators/index.blade.php
@@ -6,32 +6,39 @@
 
     <ul class="list-disc list-inside">
         @forelse($database->creators as $creator)
-					<li><b>Name</b>: {{ $creator->creatorName }}
-						@if ($creator->givenName != null) <b>Given Name</b>: {{ $creator->givenName }}@endif
-						@if($creator->familyName != null) <b>Family Name</b>: {{ $creator->familyName }}@endif
-						@if ($creator->nameIdentifier != null) <b>{{ $creator->nameIdentifierScheme($creator->nameIdentifierSchemeIndex) }}</b>: {{ $creator->nameIdentifier }}@endif
-						@if ($creator->creatorAffiliation != null) <b>Affiliation</b>: {{ $creator->creatorAffiliation }}@endif
-						@if ($creator->affiliationIdentifier != null) <b>{{ $creator->affiliationIdentifierScheme }}</b>: {{ $creator->affiliationIdentifier }}@endif
-
-						@auth
-							@if( Auth::user()->id  == $database->user_id)
-								<form class="bg-green-100 inline" action="{{ route('creators.edit', [$creator]) }}">
-									<button type="submit" class="btn btn-danger btn-sm">Edit</button>
-								</form>
-								<form class="bg-red-100 inline" method="POST" action="{{ route('creators.destroy', [$creator]) }}">
-									@csrf @method('DELETE')
-									<button type="submit" class="btn btn-danger btn-sm">Delete</button>
-								</form>
-							@endif
-						@endauth
-					</li>
+            <li><b>Name</b>: {{ $creator->creatorName }}
+                @if ($creator->givenName != null)
+                    <b>Given Name</b>: {{ $creator->givenName }}
+                @endif
+                @if ($creator->familyName != null)
+                    <b>Family Name</b>: {{ $creator->familyName }}
+                @endif
+                @if ($creator->nameIdentifier != null)
+                    <b>{{ $creator->nameIdentifierScheme($creator->nameIdentifierSchemeIndex) }}</b>:
+                    {{ $creator->nameIdentifier }}
+                @endif
+                @if ($creator->creatorAffiliation != null)
+                    <b>Affiliation</b>: {{ $creator->creatorAffiliation }}
+                @endif
+                @if ($creator->affiliationIdentifier != null)
+                    <b>{{ $creator->affiliationIdentifierScheme }}</b>: {{ $creator->affiliationIdentifier }}
+                @endif
+                @can('update', $database)
+                    <x-button method="GET" action="{{ route('creators.edit', [$creator]) }}" class="inline">
+                        Edit
+                    </x-button>
+                @endcan
+                @can('delete', $database)
+                    <x-button method="DELETE" action="{{ route('creators.destroy', [$creator]) }}" class="inline">
+                        Delete
+                    </x-button>
+                @endcan
+            </li>
         @empty
-					<li>No creators defined.</li>
-				@endforelse
+            <li>No creators defined.</li>
+        @endforelse
     </ul>
 
-		<h3>Add a new creator:</h3>
-				<livewire:creator-form :database=$database />
-
+    <livewire:creator-form :database=$database />
 
 </x-app-layout>
