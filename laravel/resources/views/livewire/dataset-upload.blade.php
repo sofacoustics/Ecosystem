6a5243e83f4bcce1b4a2cf044c11e26f65afdfb0
Jonnie Stuefer <jonnie@floorspot.org>
2025-01-31 09:33:13 +0100

    Bugfix: dataset-upload now only available for dataset owner

--- laravel/resources/views/livewire/dataset-upload.blade.php
+++ laravel/resources/views/livewire/dataset-upload.blade.php
@@ -1,5 +1,6 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
-	<input type="file" wire:model="files" multiple>
-	<button wire:click="uploadFiles">Upload Files</button>
+    @can('update', $dataset->database)
+        <input type="file" wire:model="files" multiple>
+        <button wire:click="uploadFiles">Upload Files</button>
+    @endcan
 </div>
