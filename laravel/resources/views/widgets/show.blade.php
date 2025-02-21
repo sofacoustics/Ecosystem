11a516513bd0d65aca525042966d76aee8d31ef4
Jonnie Stuefer <jonnie@floorspot.org>
2025-02-21 13:56:57 +0100

    Bugfix: only access service if there is one

--- laravel/resources/views/widgets/show.blade.php
+++ laravel/resources/views/widgets/show.blade.php
@@ -4,5 +4,7 @@
     </x-slot>
     <p>{{ $widget->description }}</p>
     <p><b>View:</b> {{ $widget->view }}</p>
+@if(!is_null($widget->service))
     <p><b>Service:</b> {{ $widget->service->name }}</p>
+@endif
 </x-app-layout>
