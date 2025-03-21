7407fadcf95f093c481730481fc1e5fda3d6d96a
Piotr Majdak <piotr@majdak.com>
2025-03-21 14:34:07 +0100

    Upload buttons added to the header, header improved

--- laravel/resources/views/components/database/header.blade.php
+++ laravel/resources/views/components/database/header.blade.php
@@ -18,20 +18,33 @@
 <!-- <p>laravel\resources\views\components\database\header.blade.php</p> --!>
 
 <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-    <a href="{{ route('databases.show', $database->id) }}">{{ $database->title }}</a>
+    <a href="{{ route('databases.show', $database->id) }}">{{ $database->title }} ({{ $database->productionyear }})</a>
     @role('admin')
-			(ID: {{ $database->id }})
+			<small>(ID: {{ $database->id }})</small>
     @endrole
 </h2>
-<x-property name="Description">
-    {{ $database->description }}
+
+<x-property name="Additional Title">
+  {{ $database->additionaltitle }}
 </x-property>
+
 <x-property name="Ecosystem Manager">
-    {{ \App\Models\User::find($database->user_id)->name }}
+  {{ \App\Models\User::find($database->user_id)->name }}
 </x-property>
-    @can('update', $database)
-        <x-button method="GET" action="{{ route('databases.edit', [$database]) }}" class="inline">Edit Metadata</x-button>
-    @endcan
-<x-button class="inline" method="GET" action="{{ route('databases.show', $database->id) }}">Datasets</x-button>
+
+@can('update', $database)
+		<x-button method="GET" action="{{ route('databases.edit', [$database]) }}" class="inline">Edit</x-button>
+@endcan
+
 <x-button class="inline" method="GET" action="{{ route('databases.datasetdefs', $database->id) }}">Definition</x-button>
 
+@if(count($database->datasetdefs))
+	<x-button class="inline" method="GET" action="{{ route('databases.show', $database->id) }}">Datasets</x-button>
+@endif
+
+@can('create', [App\Models\Dataset::class, $database])
+	@if(count($database->datasetdefs))
+		<x-button method="GET" class="inline" action="{{ route('databases.datasets.create', [$database->id]) }}">New</x-button>
+		<x-button method="GET" class="inline" action="{{ route('databases.datasets.bulkupload', [$database->id]) }}">Bulk Upload</x-button>
+	@endif
+@endcan
