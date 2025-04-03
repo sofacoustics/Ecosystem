3363d04e9c2250891894fe86e93c71b9ac84b405
Jonnie Stuefer <jonnie@floorspot.org>
2025-04-03 12:03:58 +0200

    Removing 'bulkupload.js' which we're not using

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/bulkupload.js', // will put Piotr's bulk upload code here, when it no longer has @ blade directives
                 'resources/css/filament/app/theme.css',
             ],
             refresh: true,
