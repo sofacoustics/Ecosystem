2f9854ff6bb293a38669dae9e832aecc83f9a207
Piotr Majdak <piotr@majdak.com>
2025-04-09 16:41:46 +0200

    Image view: rotation fixed, zoom removed.

--- laravel/resources/views/livewire/datafiles/image.blade.php
+++ laravel/resources/views/livewire/datafiles/image.blade.php
@@ -1,77 +1,61 @@
 <div id="imagecontainer">
 	<p>
-        <x-button type="button" onclick="zoomin()">Zoom In</x-button>
-        <x-button type="button" onclick="zoomout()">Zoom Out</x-button>
-        <x-button type="button" onclick="cw()">Rotate Clockwise</x-button>
-				<x-button type="button" onclick="acw()">Rotate Anticlockwise</x-button>
-				<x-button type="button" onclick="resetbutton()">Reset View</x-button>
+        <x-button type="button" onclick="cw{{ $datafile->id }}()">Rotate Clockwise</x-button>
+				<x-button type="button" onclick="acw{{ $datafile->id }}()">Rotate Anticlockwise</x-button>
+				<x-button type="button" onclick="resetbutton{{ $datafile->id }}()">Reset View</x-button>
   </p>
-	<img id="image" class="p-2 " style="border: 1px solid #ccc; max-width:500px; position: relative;" onClick="reset(this)" src="{{ $datafile->asset() }}" />
+	<a href="{{ $datafile->asset() }}" target="_blank">
+		<img id="image{{ $datafile->id }}" class="p-2 " 
+			style="border: 1px solid #ccc; max-width:500px; position: relative;" 
+			onClick="reset(this)" 
+			src="{{ $datafile->asset() }}" 
+		/>
+	</a>
 </div>
 
 <script>
-	function cw() 
+	var Angle = 0;
+	function cw{{ $datafile->id }}() 
 	{
-		var myImg = document.getElementById("image");
-		console.log(myImg.width);
-		console.log(myImg.height);
-		myImg.style.transform = "rotate(90deg)"; 
-		x = myImg.width/2 - myImg.height/2;
-		y = myImg.height/2 - myImg.width/2;
+		var myImg = document.getElementById("image{{ $datafile->id }}");
+		console.log(myImg.getBoundingClientRect());
+		console.log(myImg.clientHeight);
+		Angle = Angle + 90; 
+		myImg.style.transform = "rotate(" + Angle + "deg)"; 
+		rect = myImg.getBoundingClientRect();
+		x = myImg.clientWidth/2 - rect.width/2;
+		y = myImg.clientHeight/2 - rect.height/2;
 		myImg.style.top = x + "px";
 		myImg.style.left = y + "px";
 	}
 	
-	function acw() 
+	function acw{{ $datafile->id }}() 
 	{
-		var myImg = document.getElementById("image");
-		myImg.style.transform = "rotate(270deg)"; 
-		x = myImg.width/2 - myImg.height/2;
-		y = myImg.height/2 - myImg.width/2;
+		var myImg = document.getElementById("image{{ $datafile->id }}");
+		Angle = Angle - 90;
+		myImg.style.transform = "rotate(" + Angle + "deg)"; 
+		rect = myImg.getBoundingClientRect();
+		x = myImg.clientWidth/2 - rect.width/2;
+		y = myImg.clientHeight/2 - rect.height/2;
 		myImg.style.top = x + "px";
 		myImg.style.left = y + "px";
 	}
 
  	function reset(myImg)
 	{
+		myImg.style.transform = "rotate(0deg)";
+		Angle = 0;
+		myImg.style.top = "0px";
+		myImg.style.left = "0px";
+	}
+	
+ 	function resetbutton{{ $datafile->id }}()
+	{
+		var myImg = document.getElementById("image{{ $datafile->id }}");
+		Angle = 0;
 		myImg.style.transform = "rotate(0deg)"; 
 		myImg.style.top = "0px";
 		myImg.style.left = "0px";
 	}
 	
- 	function resetbutton()
-	{
-		var myImg = document.getElementById("image");
-		myImg.style.transform = "rotate(0deg)"; 
-		myImg.style.top = "0px";
-		myImg.style.left = "0px";
-	}
-	
-	function zoomin()
-	{
-		var myImg = document.getElementById("image");
-		var currWidth = myImg.clientWidth;
-		if(currWidth == 500)
-		{
-			alert("Maximum zoom-in level reached.");
-		} 
-		else
-		{
-			myImg.style.width = (currWidth + 50) + "px";
-		} 
-	}
-	
-	function zoomout()
-	{
-		var myImg = document.getElementById("image");
-		var currWidth = myImg.clientWidth;
-		if(currWidth == 50)
-		{
-			alert("Maximum zoom-out level reached.");
-		} 
-		else
-		{
-			myImg.style.width = (currWidth - 50) + "px";
-		}
-	}
 </script>
