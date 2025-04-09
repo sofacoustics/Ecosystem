3863012161f8e8e210ff218e565d10b69149aa7e
isfmiho <miho@kfs.oeaw.ac.at>
2025-04-09 14:49:46 +0200

    ServiceSeeder.php: syntax bugs fixed

--- laravel/database/seeders/ServiceSeeder.php
+++ laravel/database/seeders/ServiceSeeder.php
@@ -27,46 +27,55 @@ class ServiceSeeder extends Seeder
                     'exe' => 'octave-cli',
                     'parameters' => 'HRIR1.m'
                 )
-            );Service::create(array(
+            );
+			Service::create(array(
                     'name' => 'Octave: BRIR Geometry',
+                    'description' => 'Plot BRIR Geometry.',
                     'exe' => 'octave-cli',
                     'parameters' => 'BRIRGeometry.m'
                 )
-            ));Service::create(array(
-                    'name' => 'Octave: SRIR Geometry',
-                    'exe' => 'octave-cli',
-                    'parameters' => 'SRIRGeometry.m'
-                )
-            ));Service::create(array(
-                    'name' => 'Octave: Directivities Polar',
-                    'description' => 'Plot the directivities as polar plots.',
-                    'exe' => 'octave-cli',
-                    'parameters' => 'DirectivityPolar.m'
-                )
-            ));Service::create(array(
-                    'name' => 'Octave: SOFA Properties',
-                    'description' => 'Show SOFA Properties.',
-                    'exe' => 'octave-cli',
-                    'parameters' => 'SofaProperties.m'
-                )
-            ));Service::create(array(
-                    'name' => 'Blender: Render PPM',
-                    'description' => 'Render PPM if BezierPPM, show CSV file properties otherwise.',
-                    'exe' => 'python',
-                    'parameters' => 'CSVppm.py'
-                )
-            ));Service::create(array(
-                    'name' => 'Octave: AnnotatedReceiver',
-                    'description' => 'Plot the progress of receivers',
-                    'exe' => 'octave-cli',
-                    'parameters' => 'SOFAAnnotatedReceiver.m'
-                )
-            ));Service::create(array(
-                    'name' => 'Octave: Headphones',
-                    'description' => 'Plot spectra of headphones',
-                    'exe' => 'octave-cli',
-                    'parameters' => 'Headphones.m'
-                )
+            );
+			Service::create(array(
+                   'name' => 'Octave: SRIR Geometry',
+                   'description' => 'Plot SRIR Geometry.',
+                   'exe' => 'octave-cli',
+                   'parameters' => 'SRIRGeometry.m'
+               )
+            );
+			Service::create(array(
+                   'name' => 'Octave: Directivities Polar',
+                   'description' => 'Plot the directivities as polar plots.',
+                   'exe' => 'octave-cli',
+                   'parameters' => 'DirectivityPolar.m'
+               )
+            );
+			Service::create(array(
+                   'name' => 'Octave: SOFA Properties',
+                   'description' => 'Show SOFA Properties.',
+                   'exe' => 'octave-cli',
+                   'parameters' => 'SofaProperties.m'
+               )
+            );
+			Service::create(array(
+                   'name' => 'Blender: Render PPM',
+                   'description' => 'Render PPM if BezierPPM, show CSV file properties otherwise.',
+                   'exe' => 'python',
+                   'parameters' => 'CSVppm.py'
+               )
+            );
+			Service::create(array(
+                   'name' => 'Octave: AnnotatedReceiver',
+                   'description' => 'Plot the progress of receivers.',
+                   'exe' => 'octave-cli',
+                   'parameters' => 'SOFAAnnotatedReceiver.m'
+               )
+            );
+			Service::create(array(
+                   'name' => 'Octave: Headphones',
+                   'description' => 'Plot spectra of headphones.',
+                   'exe' => 'octave-cli',
+                   'parameters' => 'Headphones.m'
+               )
             );
     }
 }
