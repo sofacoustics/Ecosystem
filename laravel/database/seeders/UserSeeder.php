71fefe94473ad735e1a4cb7225d273ce5c589774
Piotr Majdak <piotr@majdak.com>
2025-04-11 12:09:45 +0200

    ORCID added to user profile

--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -20,25 +20,33 @@ class UserSeeder extends Seeder
         $user = User::create([
             'name' => 'Jonnie Admin',
             'email' => 'jonnie@floorspot.org',
-            'password' => '$2y$10$eljLQEcv5TU/7sgcBQZCMupOVkHPKIrPPXOgKmSs/MKlI9i.Mnp66'
+            'password' => '$2y$10$eljLQEcv5TU/7sgcBQZCMupOVkHPKIrPPXOgKmSs/MKlI9i.Mnp66',
+						'orcid' => '0000-0000-0000-0000'
            ]);
            $user->assignRole('admin');
+					 
         $user = User::create([
-            'name' => 'Piotr Admin',
+            'name' => 'Piotr Majdak',
             'email' => 'piotr.majdak@oeaw.ac.at',
-            'password' => '$2y$10$oO3j2Rv8E5qlsFlEWZMbk..qpiEcABAQkFATlXeHI.ZN/xI2LaDL.'
+            'password' => '$2y$10$oO3j2Rv8E5qlsFlEWZMbk..qpiEcABAQkFATlXeHI.ZN/xI2LaDL.',
+						'orcid' => '0000-0003-1511-6164'
            ]);
            $user->assignRole('admin');
+					 
         $user = User::create([
             'name' => 'Jonnie User',
             'email' => 'jonathan.stuefer@oeaw.ac.at',
-            'password' => '$2y$10$eljLQEcv5TU/7sgcBQZCMupOVkHPKIrPPXOgKmSs/MKlI9i.Mnp66'
+            'password' => '$2y$10$eljLQEcv5TU/7sgcBQZCMupOVkHPKIrPPXOgKmSs/MKlI9i.Mnp66',
+						'orcid' => '0000-0000-0000-0001'
         ]);
+				
         $user = User::create([
             'name' => 'Michael Mihocic',
             'email' => 'michael.mihocic@oeaw.ac.at',
-            'password' => '$2y$10$en9XzixWc7wEn25LoZOn8.M7DYodeQa7T2najcOp1h5B/OCMiPKS6'
+            'password' => '$2y$10$en9XzixWc7wEn25LoZOn8.M7DYodeQa7T2najcOp1h5B/OCMiPKS6',
+						'orcid' => '0000-0002-0800-6293'
         ]);
+           $user->assignRole('admin');
 
     }
 }
