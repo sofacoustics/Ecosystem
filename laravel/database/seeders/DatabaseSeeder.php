ca17becc268b21fa571b6a6d86c3613d77e549c6
Michael Mihocic <michael.mihocic@oeaw.ac.at>
2025-04-14 10:38:54 +0200

    DatabaseSeeder.pbp and DatasetdefSeeder.php updated

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -73,6 +73,21 @@ class DatabaseSeeder extends Seeder
 						'additionalrights' => null,
             'radardataset' => null,
             'user_id' => 4, ));
+		Database::create(array('title' => 'Test Services',
+						'additionaltitle' => null,
+            'description' => 'Specialty: SONICOM Database',
+						'productionyear' => '2025',
+						'language' => 'eng',
+						'resourcetype' => null,
+						'resource' => null,
+						'datasources' => null,
+						'software' => null,
+						'processing' => null,
+						'relatedinformation' => null,
+						'controlledrights' => 47,
+						'additionalrights' => null,
+            'radardataset' => null,
+            'user_id' => 4, ));
         //Database::create(array('name' => 'Jonnie\'s ARI SOFA test 2', 'description' => 'Nr. 2. \'Nuf said!', 'user_id' => 1, 'radar_id' => 'dEZxRRrxpiHSzbBZ'));
         $this->call([
             DatasetSeeder::class,
--- laravel/database/seeders/DatasetdefSeeder.php
+++ laravel/database/seeders/DatasetdefSeeder.php
@@ -25,6 +25,12 @@ class DatasetdefSeeder extends Seeder
 		Datasetdef::create([ 'database_id' => 3, 'name' => 'HRTF 48kHz', 'datafiletype_id' => 1, 'widget_id' => 1 ]);
 		Datasetdef::create([ 'database_id' => 3, 'name' => 'HPEQ 48kHz', 'datafiletype_id' => 1, 'widget_id' => 1 ]);
 		Datasetdef::create([ 'database_id' => 3, 'name' => 'Photogrammetry', 'datafiletype_id' => 1, 'widget_id' => 1 ]);
-		
+		Datasetdef::create([ 'database_id' => 4, 'name' => 'sofa-brir-geometry', 'datafiletype_id' => 2, 'widget_id' => 3 ]);
+		Datasetdef::create([ 'database_id' => 4, 'name' => 'sofa-srir-geometry', 'datafiletype_id' => 3, 'widget_id' => 4 ]);
+		Datasetdef::create([ 'database_id' => 4, 'name' => 'sofa-directivity-polar', 'datafiletype_id' => 4, 'widget_id' => 5 ]);
+		Datasetdef::create([ 'database_id' => 4, 'name' => 'sofa-properties', 'datafiletype_id' => 5, 'widget_id' => 6 ]);
+		Datasetdef::create([ 'database_id' => 4, 'name' => 'bezierppm', 'datafiletype_id' => 7, 'widget_id' => 7 ]);
+		Datasetdef::create([ 'database_id' => 4, 'name' => 'sofa-annotated-receiver', 'datafiletype_id' => 14, 'widget_id' => 8 ]);
+		Datasetdef::create([ 'database_id' => 4, 'name' => 'sofa-headphones', 'datafiletype_id' => 12, 'widget_id' => 9 ]);
 	}
 }
