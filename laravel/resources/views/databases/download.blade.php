b07408301cc932d6dfdf75c0a564216590ac0e7a
Piotr Majdak <piotr@majdak.com>
2025-05-06 22:22:59 +0200

    download: database id added, layout improved

--- laravel/resources/views/databases/download.blade.php
+++ laravel/resources/views/databases/download.blade.php
@@ -2,18 +2,16 @@
 	<x-slot name="header">
 		<x-database.header :database=$database />
 	</x-slot>
-	<h3>JSON Format:</h3>
-	<p> 
+	<h3>Database ID: {{ $database->id }}
+	<h3>Database list (JSON Format):</h3>
+	<p><small> 
 		<a href="{{ route('databases.download', ['database' => $database->id, 'type' => 'json']) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block">
 			Download
 		</a>
-		the datafile list in JSON format.
-	</p>
-	<p>
-		<button id="copyButton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Copy</button>
+		
+		<button id="copyButton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Copy link to clipboard</button>
 		<input type="text" id="textToCopy" value="{{ route('databases.download', ['database' => $database->id, 'type' => 'json']) }}" class="hidden">
-		the link to the datafile list to the clipboard.
-	</p>
+	</small></p>
     <script>
     document.getElementById('copyButton').addEventListener('click', function() {
         // Get the text from the input field
