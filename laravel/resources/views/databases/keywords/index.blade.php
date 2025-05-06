dbe6cc85a32c4174b9b2c16ce78b87f22da70091
Piotr Majdak <piotr@majdak.com>
2025-05-06 21:57:37 +0200

    keywords layout fixes

--- laravel/resources/views/databases/keywords/index.blade.php
+++ laravel/resources/views/databases/keywords/index.blade.php
@@ -8,36 +8,23 @@
 		
 	<ul class="list-disc list-inside">
 		@forelse($database->keywords as $keyword)
-					<li>
-						@can('update', $database)
-							<x-button method="GET" action="{{ route('keywords.edit', [$keyword]) }}" class="inline">
-								Edit
-							</x-button>
-						@endcan
-						@can('delete', $database)
-							<x-button method="DELETE" action="{{ route('keywords.destroy', [$keyword]) }}" class="inline">
-								Delete
-							</x-button>
-						@endcan
-						<b> {{ $keyword->keywordName }} </b> (
-						@if ($keyword->schemeURI != null)<a href="{{ $keyword->schemeURI }}"> @endif
-							{{ \App\Models\Keyword::keywordScheme($keyword->keywordSchemeIndex)}}
-						@if ($keyword->schemeURI != null)</a> @endif </b>: 
-						@if ($keyword->valueURI != null)<a href="{{ $keyword->valueURI }}"> @endif
-						{{ $keyword->classificationCode }}
-						@if ($keyword->valueURI != null)</a> @endif )
-					</li>
-
-							@can('update', $database)
-									<x-button method="GET" action="{{ route('keywords.edit', [$keyword]) }}" class="inline">
-											Edit
-									</x-button>
-							@endcan
-							@can('delete', $database)
-									<x-button method="DELETE" action="{{ route('keywords.destroy', [$keyword]) }}" class="inline">
-											Delete
-									</x-button>
-							@endcan
+			<li>
+				@can('update', $database)
+					<x-button method="GET" action="{{ route('keywords.edit', [$keyword]) }}" class="inline">
+						Edit
+					</x-button>
+				@endcan
+				@can('delete', $database)
+					<x-button method="DELETE" action="{{ route('keywords.destroy', [$keyword]) }}" class="inline">
+						Delete
+					</x-button>
+				@endcan
+				<b>{{ $keyword->keywordName }}</b> 
+				@if($keyword->classificationCode)
+					(@if ($keyword->schemeURI != null)<a href="{{ $keyword->schemeURI }}">@endif{{ \App\Models\Keyword::keywordScheme($keyword->keywordSchemeIndex)}}@if ($keyword->schemeURI != null)</a>@endif: 
+					@if ($keyword->valueURI != null)<a href="{{ $keyword->valueURI }}">@endif{{ $keyword->classificationCode }}@if ($keyword->valueURI != null)</a>@endif)
+				@endif
+			</li>
 		  </li>
 		@empty
 			<li>No keywords defined yet.</li>
--- laravel/resources/views/databases/show.blade.php
+++ laravel/resources/views/databases/show.blade.php
@@ -121,13 +121,12 @@
 		@endcan
 		<ul class="list-disc list-inside">
 			@forelse ($database->keywords as $keyword)
-			<li><b> {{ $keyword->keywordName }} </b> (
-				@if ($keyword->schemeURI != null)<a href="{{ $keyword->schemeURI }}"> @endif
-					{{$keyword->keywordScheme($keyword->keywordSchemeIndex)}}
-				@if ($keyword->schemeURI != null)</a> @endif </b>: 
-				@if ($keyword->valueURI != null)<a href="{{ $keyword->valueURI }}"> @endif
-				{{ $keyword->classificationCode }}
-				@if ($keyword->valueURI != null)</a> @endif )
+			<li>
+				<b>{{ $keyword->keywordName }}</b> 
+				@if($keyword->classificationCode)
+					(@if ($keyword->schemeURI != null)<a href="{{ $keyword->schemeURI }}">@endif{{ \App\Models\Keyword::keywordScheme($keyword->keywordSchemeIndex)}}@if ($keyword->schemeURI != null)</a>@endif: 
+					@if ($keyword->valueURI != null)<a href="{{ $keyword->valueURI }}">@endif{{ $keyword->classificationCode }}@if ($keyword->valueURI != null)</a>@endif)
+				@endif
 			</li>
 			@empty
 				@cannot('update', $database)
