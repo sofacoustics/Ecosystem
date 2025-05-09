2daf97627a333f909d47fda221baca167d6e4142
Jonnie Stuefer <jonnie@floorspot.org>
2025-05-09 11:55:16 +0200

    Changing datafile and widget id

--- laravel/tinker_queue_env.php
+++ laravel/tinker_queue_env.php
@@ -1,10 +1,11 @@
 <?php
     use App\Models\User;
     use App\Models\Datafile;
+    use App\Models\Service;
     use App\Models\Widget;
     use App\Events\Test;
 
     $user = User::find(1);
-    $datafile = Datafile::find(2);
-    $widget = Widget::find(1);
+    $datafile = Datafile::find(57);
+    $widget = Widget::find(6);
 ?>
