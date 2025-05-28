c680f8a0c162f9f55c703d4cbf1fd9f2dd84759c
Jonnie Stuefer <jonnie@floorspot.org>
2025-05-28 11:56:32 +0200

    Added RADAR .env settings to services config. Use config() instead of env()

--- laravel/config/services.php
+++ laravel/config/services.php
@@ -2,40 +2,50 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Third Party Services
+	|--------------------------------------------------------------------------
+	|
+	| This file is for storing the credentials for third party services such
+	| as Mailgun, Postmark, AWS and more. This file provides the de facto
+	| location for this type of information, allowing packages to have
+	| a conventional file to locate the various service credentials.
+	|
+	*/
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
-    ],
+	'mailgun' => [
+		'domain' => env('MAILGUN_DOMAIN'),
+		'secret' => env('MAILGUN_SECRET'),
+		'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+		'scheme' => 'https',
+	],
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
+	'postmark' => [
+		'token' => env('POSTMARK_TOKEN'),
+	],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
+	'ses' => [
+		'key' => env('AWS_ACCESS_KEY_ID'),
+		'secret' => env('AWS_SECRET_ACCESS_KEY'),
+		'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+	],
 
-		'orcid' => [
-			'api_url' => env('ORCID_API_URL', 'https://orcid.org/'), // Adjust for sandbox: 'https://sandbox.orcid.org/v3.0'
-			'client_id' => env('ORCID_CLIENT_ID'),
-			'client_secret' => env('ORCID_CLIENT_SECRET'),
-			// 'redirect' => env('ORCID_REDIRECT_URI', '/orcid/callback'), // You'll define the route
-],
+	'orcid' => [
+		'api_url' => env('ORCID_API_URL', 'https://orcid.org/'), // Adjust for sandbox: 'https://sandbox.orcid.org/v3.0'
+		'client_id' => env('ORCID_CLIENT_ID'),
+		'client_secret' => env('ORCID_CLIENT_SECRET'),
+		// 'redirect' => env('ORCID_REDIRECT_URI', '/orcid/callback'), // You'll define the route
+	],
+
+	'radar' => [
+		'clientid' => env('RADAR_CLIENTID'),
+		'clientsecret' => env('RADAR_CLIENTSECRET'),
+		'username' => env('RADAR_USERNAME'),
+		'userpassword' => env('RADAR_USERPASSWORD'),
+		'redirecturl' => env('RADAR_REDIRECTURL'),
+		'workspace' => env('RADAR_WORKSPACE'),
+		'baseurl' => env('RADAR_BASEURL'),
+	],
 
 ];
