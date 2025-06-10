9d7623d903adfca26720e16a699a7eff31d3a0ec
Jonnie Stuefer <jonnie@floorspot.org>
2025-06-10 11:47:25 +0200

    Adding middleware EnsureAdminOnly to protect routes via ->middleware('admin')

--- laravel/app/Http/Kernel.php
+++ laravel/app/Http/Kernel.php
@@ -53,7 +53,8 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
+		'admin' => \App\Http\Middleware\EnsureAdminOnly::class,
+		'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
