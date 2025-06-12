88437ab2e66d86fe405f8291351d67dbc21e2b6b
Piotr Majdak <piotr@majdak.com>
2025-06-12 10:34:30 +0200

    small user interface fixes

--- laravel/resources/views/datafiles/show.blade.php
+++ laravel/resources/views/datafiles/show.blade.php
@@ -1,35 +1,32 @@
 <x-app-layout>
-    <x-slot name="header">
-        <x-slot name="header">
+	<x-slot name="header">
+		<x-slot name="header">
 			<x-database.header :datafile="$datafile" tabTitle="Datafile | {{ $datafile->name }}"/>
-        </x-slot>
-        <x-property name="Dataset">
-            <a href="{{ route('datasets.show', $datafile->dataset->id) }}">{{ $datafile->dataset->name }}</a><br>
-        </x-property>
-        <x-property name="Datafile name">
-            {{ $datafile->name }}
-        </x-property>
-        <x-property name="Datafiletype">
-            {{ $datafile->datasetdef->datafiletype->name }}<br>
-        </x-property>
-		@hasrole('admin')
-        <x-property name="RADAR id">
-			{{ $datafile->radar_id }}
-        </x-property>
-		@endhasrole
+		</x-slot>
+		<x-property name="Dataset">
+			<a href="{{ route('datasets.show', $datafile->dataset->id) }}">{{ $datafile->dataset->name }}</a><br>
+		</x-property>
+		<x-property name="Datafile name">{{ $datafile->name }}</x-property>
+		<x-property name="Datafiletype">{{ $datafile->datasetdef->datafiletype->name }}<br></x-property>
 
-        {{-- call view based on datafiletype --}}
-        @livewire(DatafileListener::class, ['datafile' => $datafile])
+		@if($datafile->radar_id)
+			@hasrole('admin')
+				<x-property name="RADAR id">{{ $datafile->radar_id }}</x-property>
+			@endhasrole
+		@endif
 
-		<x-button action="{{  asset($datafile->url()) }}">Download</x-button>
-		@can('update', $datafile)
-			@if($datafile->dataset->database->radar_id != null && $datafile->radar_id == null)
-				<x-button loadingText="Uploading: please wait" method="POST" action="{{ route('datafiles.uploadtoradar', $datafile) }}">Upload to RADAR</x-button>
-			@endif
-		@endcan
-		@can('delete', $datafile)
-			@if($datafile->radar_id)
-				<x-button loadingText="Deleting: please wait" method="DELETE" action="{{ route('datafiles.deletefromradar', $datafile) }}">Delete from RADAR</x-button>
-			@endif
-		@endcan
+		{{-- call view based on datafiletype --}}
+		@livewire(DatafileListener::class, ['datafile' => $datafile])
+
+	<x-button action="{{  asset($datafile->url()) }}">Download</x-button>
+	@can('update', $datafile)
+		@if($datafile->dataset->database->radar_id != null && $datafile->radar_id == null)
+			<x-button loadingText="Uploading: please wait" method="POST" action="{{ route('datafiles.uploadtoradar', $datafile) }}">Upload to RADAR</x-button>
+		@endif
+	@endcan
+	@can('delete', $datafile)
+		@if($datafile->radar_id)
+			<x-button loadingText="Deleting: please wait" method="DELETE" action="{{ route('datafiles.deletefromradar', $datafile) }}">Delete from RADAR</x-button>
+		@endif
+	@endcan
 </x-app-layout>
--- laravel/resources/views/datasets/show.blade.php
+++ laravel/resources/views/datasets/show.blade.php
@@ -1,50 +1,42 @@
 <x-app-layout>
-    <x-slot name="header">
-		<x-database.header :dataset="$dataset" tabTitle="Dataset | {{ $dataset->name }}"/>
-    </x-slot>
-    <h2>
-        Dataset: {{ $dataset->name }} @role('admin')
-            (ID: {{ $dataset->id }})
-        @endrole
-    </h2>
-    <p>This dataset contains {{ count($dataset->datafiles) }} files.</p>
-    <div class="ml-2">
-			@foreach ($dataset->datafiles as $datafile)
-				<h3>Name: {{ $datafile->datasetdef->name }}
-					@can('delete', $datafile)
-						<x-button method="DELETE" class="inline" action="{{ route('datafiles.destroy', [$datafile]) }}">Delete</x-button>
-					@endcan
-				</h3>
-				<div wire:key="{{ $datafile->id }}">
-					<x-property name="Datafile Name">
-						<a href="{{ route('datafiles.show', $datafile->id) }}">{{ $datafile->name }}</a> @role('admin') (ID: {{ $datafile->id }}) @endrole
-					</x-property>
-					<livewire:DatafileListener :datafile="$datafile" :key="$datafile->id" />
-				</div>
-				<hr>
+	<x-slot name="header">
+	<x-database.header :dataset="$dataset" tabTitle="Dataset | {{ $dataset->name }}"/>
+	</x-slot>
+	<h2>
+		Dataset: {{ $dataset->name }} @role('admin')
+			(ID: {{ $dataset->id }})
+		@endrole
+	</h2>
+	<p>This dataset contains {{ count($dataset->datafiles) }} files.</p>
+	<div class="ml-2">
+		@foreach ($dataset->datafiles as $datafile)
+			<h3>Name: {{ $datafile->datasetdef->name }}
+				@can('delete', $datafile)
+					<x-button method="DELETE" class="inline" action="{{ route('datafiles.destroy', [$datafile]) }}">Delete</x-button>
+				@endcan
+			</h3>
+			<div wire:key="{{ $datafile->id }}">
+				<x-property name="Datafile Name">
+					<a href="{{ route('datafiles.show', $datafile->id) }}">{{ $datafile->name }}</a> @role('admin') (ID: {{ $datafile->id }}) @endrole
+				</x-property>
+				<livewire:DatafileListener :datafile="$datafile" :key="$datafile->id" />
+			</div>
+			<hr>
+		@endforeach
+	</div>
+
+	@if (count($dataset->datafiles) < count($dataset->database->datasetdefs))
+		<p>According to the dataset definition, the following
+			{{ count($dataset->database->datasetdefs) - count($dataset->datafiles) }} files are missing:</p>
+
+		<ul class="list-disc list-inside">
+			@foreach ($dataset->missing_datafiles() as $missing)
+				<li>
+					<x-datasetdef.list :datasetdef="$missing" />
+					<livewire:datafile-upload :dataset=$dataset :datasetdef=$missing :wire:key="$missing->id" />
+				</li>
 			@endforeach
-    </div>
-
-    @if (count($dataset->datafiles) < count($dataset->database->datasetdefs))
-			<p>According to the dataset definition, the following
-					{{ count($dataset->database->datasetdefs) - count($dataset->datafiles) }} files are missing:</p>
-
-			<ul class="list-disc list-inside">
-				@foreach ($dataset->missing_datafiles() as $missing)
-					<li>
-						<x-datasetdef.list :datasetdef="$missing" />
-						<livewire:datafile-upload :dataset=$dataset :datasetdef=$missing :wire:key="$missing->id" />
-					</li>
-				@endforeach
-			</ul>
-    @endif
-
-	@can('update', $dataset)
-		@if($dataset->radar_id == null)
-			<x-button loadingText="Uploading: please be patient" method="POST" action="{{ route('datasets.uploadtoradar', $dataset) }}">Upload to RADAR</x-button>
-		@else
-			<x-button loadingText="Deleting: please be patient" method="DELETE" action="{{ route('datasets.deletefromradar', $dataset) }}">Delete from RADAR</x-button>
-		@endif
+		</ul>
 	@endif
 
 </x-app-layout>
