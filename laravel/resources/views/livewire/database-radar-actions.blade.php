50a220452b85b3a61706e9c9c351ec0f5c963c93
Jonnie Stuefer <jonnie@floorspot.org>
2025-06-12 14:33:21 +0200

    Rebase conflict fix

--- laravel/resources/views/livewire/database-radar-actions.blade.php
+++ laravel/resources/views/livewire/database-radar-actions.blade.php
@@ -8,29 +8,27 @@
 
 	@can('own', $database)
 		@if($id == null)
-			<x-button wire:click="createDataset">Create RADAR Dataset</x-button>
+			<x-livewire-button wire:click="createDataset" loading="Creating...">Create RADAR Dataset</x-livewire-button>
 		@endif
 		@if($pending)
-			<x-button wire:click="startReview">Start Review</x-button>
+			<x-livewire-button wire:click="startReview" loading="Starting...">Start Review</x-livewire-button>
 		@endif
 		@if($review)
-			<x-button wire:click="endReview">End Review</x-button>
+			<x-livewire-button wire:click="endReview" loading="Ending...">End Review</x-livewire-button>
 		@endif
 		@if($canUpload)
-			{{--
 			<x-livewire-button wire:click="uploadToRadar" loading="Uploading...">Upload to RADAR</x-livewire-button>
-			--}}
-			<x-button wire:click="uploadToRadar">Upload to RADAR</x-button>
 		@endif
 
 		<x-button wire:click="resetDOI">Reset DOI and the Persistent Publication</x-button>
 
 		@if($radarstatus == 2)
 			<x-button wire:click="approvePublication">Approve the Persistent Publication</x-button>
+			<x-livewire-button wire:click="resetDOI" loading="Resetting...">Reset DOI and the Persistent Publication</x-livewire-button>
 		@endif
 
 		@if(isset($id))
-			<x-button method="DELETE" wire:click="deleteFromRadar">Delete from RADAR</x-button>
+			<x-livewire-button style='delete' wire:click="deleteFromRadar" loading="Deleting...">Delete from RADAR</x-livewire-button>
 		@endif
 	@endcan
 	@if($error)
