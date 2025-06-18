a1c2c77ebb36ac26d3481d394ae1d6e9d2bb24ed
Jonnie Stuefer <jonnie@floorspot.org>
2025-06-18 09:33:57 +0200

    Bugfix: adding missing livewire-button component

---
+++ laravel/resources/views/components/livewire-button.blade.php
@@ -0,0 +1,48 @@
+{{--
+	A button for use in a Livewire blade
+
+	Call with, e.g.
+
+		<x-button wire:click="save" loading="Saving...">
+			Save
+		</x-button>
+
+	Or for form submission (outside of Livewire):
+
+		<form method="POST" action="/submit">
+			@csrf
+			<x-button>
+				Submit
+			</x-button>
+		</form>
+--}}
+
+@props([
+    'loading' => null, // Optional: loading text, e.g., "Saving..."
+    'type' => 'submit', // Default to submit, can be overridden
+	'style' => 'submit', // Default 'submit'. Possible values: submit, delete
+])
+
+@php
+    // Determine the Livewire action (if any) for loading targeting
+    $wireClick = $attributes->wire('click')->value();
+	if($style == 'delete')
+		$classColors = $buttonColorDelete;
+	else
+		$classColors = $buttonColorEnabled;
+	$buttonClass = "$classColors $buttonStyle";
+@endphp
+
+<button
+    {{ $attributes->merge(['type' => $type, 'class' => $buttonClass]) }}
+    @if($wireClick) wire:loading.attr="disabled" @endif
+	wire:loading.class.remove="{{ $classColors }}"
+	wire:loading.class="{{ $buttonColorDisabled }}"
+>
+    @if ($loading && $wireClick)
+        <span wire:loading.remove wire:target="{{ $wireClick }}">{{ $slot }}</span>
+        <span wire:loading wire:target="{{ $wireClick }}">{{ $loading }}</span>
+    @else
+        {{ $slot }}
+    @endif
+</button>
