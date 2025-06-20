5f835bf8f6105f413cdee8cbad48d5346c34a37d
Piotr Majdak <piotr@majdak.com>
2025-06-20 19:36:33 +0200

    Confirmation on delete of the database added.

--- laravel/resources/views/components/database/header.blade.php
+++ laravel/resources/views/components/database/header.blade.php
@@ -79,7 +79,10 @@
 			@endif
 		@endcan
 		@can('delete', $database)
-			<x-button method="DELETE" action="{{ route('databases.destroy', [$database->id]) }}" class="inline">Delete Database</x-button>
+			<x-button method="DELETE" action="{{ route('databases.destroy', [$database->id]) }}" class="inline"
+				confirm="Are you sure to delete the Database?">
+				Delete Database
+			</x-button>
 		@endcan
 	@endcan
 	
