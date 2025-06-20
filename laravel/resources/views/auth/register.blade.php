55a9d70fd8ed5b83aabc17d1c0093a35ffb836bb
Piotr Majdak <piotr@majdak.com>
2025-06-20 18:37:52 +0200

    Delete of account removed. A warning for registrations added.

--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -1,52 +1,57 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+	<h3>Welcome to the SONICOM Ecosystem Registration!</h3>
+	<p>Note that if registered, your account <b>cannot be deleted</b> because of your potential contribution of persistent data to the Ecosystem.</p>
+	<p>By continuing with the registration, <b>you agree to obtain a permanent and non-deletable Ecosystem account.</b></p>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+	<form method="POST" action="{{ route('register') }}">
+		@csrf
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+		<!-- Name -->
+		<div>
+				<x-input-label for="name" :value="__('Name')" />
+				<x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+				<x-input-error :messages="$errors->get('name')" class="mt-2" />
+		</div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+		<!-- Email Address -->
+		<div class="mt-4">
+				<x-input-label for="email" :value="__('Email')" />
+				<x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+				<x-input-error :messages="$errors->get('email')" class="mt-2" />
+		</div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+		<!-- Password -->
+		<div class="mt-4">
+			<x-input-label for="password" :value="__('Password')" />
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+			<x-text-input id="password" class="block mt-1 w-full"
+											type="password"
+											name="password"
+											required autocomplete="new-password" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+			<x-input-error :messages="$errors->get('password')" class="mt-2" />
+		</div>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+		<!-- Confirm Password -->
+		<div class="mt-4">
+			<x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+			<x-text-input id="password_confirmation" class="block mt-1 w-full"
+											type="password"
+											name="password_confirmation" required autocomplete="new-password" />
 
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+			<x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+		</div>
+
+		<div class="flex items-center justify-end mt-4">
+			<a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+					{{ __('Already registered?') }}
+			</a>
+
+			<x-primary-button class="ml-4">
+					{{ __('Register') }}
+			</x-primary-button>
+		</div>
+	</form>
 </x-guest-layout>
