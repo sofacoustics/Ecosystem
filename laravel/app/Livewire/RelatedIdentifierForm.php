4b127b532a72c06a3a2830a86a4255b026f7e203
Piotr Majdak <piotr@majdak.com>
2025-06-22 01:04:40 +0200

    related identifiers: layout ready for functionality

--- laravel/app/Livewire/RelatedIdentifierForm.php
+++ laravel/app/Livewire/RelatedIdentifierForm.php
@@ -15,9 +15,8 @@ class RelatedIdentifierForm extends Component
 	public $relatedidentifier;
 	public $name;
 	public $relatedidentifiertype;
-	public $relatedidentifier_base_id; 
 	public $relationtype;
-	public $relation_base_id; 
+	public $activeTab; 
 
 	protected $rules = [
 		'name' => ['required','max:255'],
@@ -34,29 +33,34 @@ class RelatedIdentifierForm extends Component
 
 	public function mount($relatedidentifierable, $relatedidentifier = null)
 	{
-		$relatedidentifier_base_id = (\App\Models\Metadataschema::where('name', 'relatedIdentifierType')->first()->id);
-		$relation_base_id = (\App\Models\Metadataschema::where('name', 'relationType')->first()->id);
-
 		$this->relatedidentifierable = $relatedidentifierable;
-		$this->relatedidentifier_base_id = $relatedidentifier_base_id;
-		$this->relation_base_id = $relation_base_id;
 		if($relatedidentifier)
 		{
 			$this->relatedidentifier = $relatedidentifier;
 			$this->relatedidentifierable_id = $relatedidentifier->relatedidentifierable_id;
 			$this->relatedidentifierable_type = $relatedidentifier->relatedidentifierable_type;
 			$this->name = $relatedidentifier->name;
-			$this->relatedidentifiertype = $relatedidentifier->relatedidentifiertype - $relatedidentifier_base_id;
-			$this->relationtype = $relatedidentifier->relationtype - $relation_base_id;
+			$this->relatedidentifiertype = $relatedidentifier->relatedidentifiertype;
+			$this->relationtype = $relatedidentifier->relationtype;
+			$this->activeTab = 'general'; 
 		}
 		else
 		{
 			$this->relatedidentifierable_id = $relatedidentifierable->id;
 			$this->relatedidentifierable_type = get_class($relatedidentifierable);
+			$this->relationtype = (\App\Models\Metadataschema::where('name', 'relationType')->where('value', 'IS_COMPILED_BY')->first()->id); 
+			$this->relatedidentifiertype = (\App\Models\Metadataschema::where('name', 'relatedIdentifierType')->where('value', 'URL')->first()->id); 
+			$this->activeTab = 'ecosystem'; 
 		}
 	}
 
-	public function save()
+	public function selectTab($tab)
+	{
+		$this->activeTab = $tab;
+	}
+
+
+	public function saveGeneral()
 	{
 		$this->validate();
 
@@ -74,7 +78,7 @@ class RelatedIdentifierForm extends Component
 		}
 		else
 		{	 
-			$this->relatedidentifier->relatedidentifiertype = $this->relatedidentifiertype + ($this->relatedidentifier_base_id); 
+			$this->relatedidentifier->relatedidentifiertype = $this->relatedidentifiertype; 
 		}
 		if ($this->relationtype == null)
 		{		// Other if not defined
@@ -82,13 +86,13 @@ class RelatedIdentifierForm extends Component
 		}
 		else
 		{	 
-			$this->relatedidentifier->relationtype = $this->relationtype + ($this->relation_base_id); 
+			$this->relatedidentifier->relationtype = $this->relationtype; 
 		}
 		$this->relatedidentifier->name = $this->name;
 		
 		$this->relatedidentifier->save();
 
-    session()->flash('message', $isNew ? 'subject area created successfully.' : 'subject area updated successfully.');
+		session()->flash('message', $isNew ? 'related identifier created successfully.' : 'related identifier updated successfully.');
 
 		if($this->relatedidentifierable_type === 'App\Models\Database')
 		{
@@ -101,6 +105,53 @@ class RelatedIdentifierForm extends Component
 			return redirect()->route('tools.relatedidentifiers',[ 'tool' => $this->relatedidentifierable->id ]);
 		}
 	}
+
+	public function saveEcosystem()
+	{
+		$this->validate();
+
+		$isNew = !$this->relatedidentifier;
+
+		if($isNew)
+		{
+			$this->relatedidentifier = new RelatedIdentifier();
+		}
+		$this->relatedidentifier->relatedidentifierable_id = $this->relatedidentifierable_id;
+		$this->relatedidentifier->relatedidentifierable_type = $this->relatedidentifierable_type;
+		if ($this->relatedidentifiertype == null)
+		{		// Other if not defined
+			$this->relatedidentifier->relatedidentifiertype = (\App\Models\Metadataschema::where('name', 'relatedIdentifierType')->where('value', 'OTHER')->first()->id); 
+		}
+		else
+		{	 
+			$this->relatedidentifier->relatedidentifiertype = $this->relatedidentifiertype; 
+		}
+		if ($this->relationtype == null)
+		{		// Other if not defined
+			$this->relatedidentifier->relationtype = (\App\Models\Metadataschema::where('name', 'relationType')->where('value', 'OTHER')->first()->id); 
+		}
+		else
+		{
+			$this->relatedidentifier->relationtype = $this->relationtype; 
+		}
+		$this->relatedidentifier->name = $this->name;
+		
+		$this->relatedidentifier->save();
+
+		session()->flash('message', $isNew ? 'related identifier created successfully.' : 'related identifier updated successfully.');
+
+		if($this->relatedidentifierable_type === 'App\Models\Database')
+		{
+			\App\Models\Database::find($this->relatedidentifier->relatedidentifierable_id)->touch();
+			return redirect()->route('databases.relatedidentifiers',[ 'database' => $this->relatedidentifierable->id ]);
+		}
+		else
+		{
+			\App\Models\Tool::find($this->relatedidentifier->relatedidentifierable_id)->touch();
+			return redirect()->route('tools.relatedidentifiers',[ 'tool' => $this->relatedidentifierable->id ]);
+		}
+	}
+
 
 	public function render()
 	{
--- laravel/resources/views/livewire/related-identifier-form.blade.php
+++ laravel/resources/views/livewire/related-identifier-form.blade.php
@@ -1,50 +1,88 @@
 @php
 	// https://laravel.com/docs/11.x/blade#conditional-classes
-	$labelClass = 'text-gray-700 mb-2 block font-bold';
-	$selectClass = 'form-control text-gray-700 rounded-lg mb-2 block font-bold';
-	$inputClass = 'text-gray-700 w-full rounded-lg border px-3 py-2 focus:outline-none';
+	$labelClass = 'text-gray-700 mb-2 inline font-bold px-3';
+	$selectClass = 'text-gray-700 mb-2 font-bold';
+	$inputClass = 'text-gray-700 flex-grow min-w-0 border px-3 mb-2';
 	$buttonClass = 'bg-blue-500 hover:bg-blue-700 rounded px-4 py-2 font-bold text-white';
+	$tabClass = 'bg-blue-500 hover:bg-blue-700 rounded font-bold text-white mx-1 my-1 py-1 px-2';
+	$tabDisabledClass = 'gray-400 bg-gray-400 text-white rounded font-bold mx-1 my-1 py-1 px-2';
 @endphp
 <div>
 	@if ($relatedidentifier) 
-		<h3>Edit a relation:</h3>
+		<h3>Edit the relation:</h3>
 	@else
 		<h3>Add a new relation:</h3>
 	@endif
-	<form wire:submit.prevent="save">
-		<div class="block">
-					
-			<label class="{{ $labelClass }}" for="relationtype">Relation Type:</label>
-			<select wire:model.live="relationtype">
-				<option value="">Select a relation...</option>
-				@foreach(\App\Models\Database::relationList() as $r => $t)
-						<option value="{{ $r }}">{{ $t->display }}</option>
-				@endforeach
-			</select>
-			@error('relationtype')
-				<span class="text-red-500">{{ $message }}</span>
-			@enderror
-			
-			<input wire:model="name" type="text" 
-				placeholder="The identifier, e.g., the DOI (if type is DOI) or an URL (if type is URL)."
-				id="name"
-				class="text-gray-700 w-full rounded-lg border px-3 py-2 focus:outline-none"/>
-			@error('name') <span class="text-red-500">{{ $message }}</span> @enderror
+	<div class="mx-auto shadow-md box-border">
 
-			<label class="{{ $labelClass }}" for="relatedidentifiertype">Identifier Type:</label>
-			<select wire:model.live="relatedidentifiertype">
-				<option value="">Select the type of identifier...</option>
-				@foreach(\App\Models\Database::relatedidentifierList() as $r => $t)
-						<option value="{{ $r }}">{{ $t->display }}</option>
-				@endforeach
-			</select>
-			@error('relatedidentifiertype') <span class="text-red-500">{{ $message }}</span> @enderror
-
-		<div class="mt-4">
-			<button type="submit" class="{{ $buttonClass }}">
-				{{ $relatedidentifier ? 'Update Relation' : 'Create Relation' }}
+			<button wire:click="selectTab('ecosystem')"
+				class="focus:outline-none transition duration-150 ease-in-out
+					{{ $activeTab === 'ecosystem' ? $tabClass : $tabDisabledClass }}">
+				Relation within the Ecosystem
 			</button>
-		</div>
+			<button wire:click="selectTab('general')"
+				class="focus:outline-none transition duration-150 ease-in-out
+				 {{ $activeTab === 'general' ? $tabClass : $tabDisabledClass }}">
+				General relation
+			</button>
+		<div class="mt-6 p-6">
+			@if($activeTab === 'ecosystem')
+				<form wire:submit.prevent="saveEcosystem">
+					<div class="block">
+						<label class="{{ $labelClass }}" for="ecosystemtype">Relation Type:</label>
+						<select wire:model.live="ecosystemtype">
+							<option value="">Select a relation...</option>
+							@foreach(\App\Models\Metadataschema::list_ids('relatedIdentifierType') as $t)
+								<option value="{{ $t->id }}">{{ \App\Models\Metadataschema::display($t->id) }}</option>
+							@endforeach
+						</select>
+						@error('ecosystemtype')
+							<span class="text-red-500">{{ $message }}</span>
+						@enderror
+						
+						<div class="mt-4">
+							<button type="submit" class="{{ $buttonClass }}">
+								{{ $relatedidentifier ? 'Update Relation' : 'Create Relation' }}
+							</button>
+						</div>
+					</div>
+				</form>
+			@endif
+				
+			@if($activeTab === 'general')
+				<form wire:submit.prevent="saveGeneral">
+					<div class="flex items-center">
+						<label class="{{ $labelClass }}" for="relationtype">Database:</label>
+						<select wire:model.live="relationtype" class="{{ $selectClass }}>
+							<option value="">Select a relation...</option>
+							@foreach(\App\Models\Metadataschema::list_ids('relationType') as $t)
+								<option value="{{ $t->id }}">{{ \App\Models\Metadataschema::display($t->id) }}</option>
+							@endforeach
+						</select>
+						@error('relationtype')
+							<span class="text-red-500">{{ $message }}</span>
+						@enderror
+						
+						<input wire:model="name" type="text" 
+							placeholder="The identifier, e.g., the DOI (if type is DOI) or an URL (if type is URL)."
+							id="name"
+							class="{{ $inputClass }}"/>
+						@error('name') <span class="text-red-500">{{ $message }}</span> @enderror
 
-	</form>
+						<select wire:model.live="relatedidentifiertype" class="{{ $selectClass }}">
+							@foreach(\App\Models\Metadataschema::list_ids('relatedIdentifierType') as $t)
+								<option value="{{ $t->id }}">{{ \App\Models\Metadataschema::display($t->id) }}</option>
+							@endforeach
+						</select>
+						@error('relatedidentifiertype') <span class="text-red-500">{{ $message }}</span> @enderror
+					</div>
+					<div class="mt-4">
+						<button type="submit" class="{{ $buttonClass }}">
+							{{ $relatedidentifier ? 'Update Relation' : 'Create Relation' }}
+						</button>
+					</div>
+				</form>
+			@endif
+		</div>
+	</div>
 </div>
