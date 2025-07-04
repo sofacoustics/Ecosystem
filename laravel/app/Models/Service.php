4431a0c6ca218134af62aedec9e2d70777159d21
Jonnie Stuefer <jonnie@floorspot.org>
2025-07-04 15:22:12 +0200

    Adding ServiceLog model which saves log message from each service process

--- laravel/app/Models/Service.php
+++ laravel/app/Models/Service.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Service extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	public function logs(): HasMany
+	{
+		return $this->hasMany(ServiceLog::class);
+	}
+
+	public function latestLog(): HasOne
+	{
+		    return $this->logs()->one()->latestOfMany();
+	}
 }
