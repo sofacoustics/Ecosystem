0c00d4140aef5329de1da1db7c41be2b4678dcd5
Piotr Majdak <piotr@majdak.com>
2025-07-04 18:09:04 +0200

    BezierPPM: Viewer adapted, BezierPPM works now.

--- laravel/resources/views/livewire/datafiles/bezierppm.blade.php
+++ laravel/resources/views/livewire/datafiles/bezierppm.blade.php
@@ -1,7 +1,6 @@
 <div id="imagecontainer">
 	<x-servicelog :log="$this->latestLog"></x-servicelog>
 	<script src="/js/stl_viewer/stl_viewer.min.js"></script>
-	{{ $datafile->asset('_1.stl') }}
 	<div id="stl_cont" style="width:500px;height:500px;margin:0 auto; text-align:left;"></div>
 	
 	<script>
@@ -11,7 +10,7 @@
 			{
 				models:
 				[
-					{filename:"{{ $datafile->asset('_1.stl') }}"}
+					{filename:"{{ $datafile->asset('_1.stl','notimestamp') }}"}
 				]
 			}
 		);
