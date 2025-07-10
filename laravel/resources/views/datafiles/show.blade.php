cdb83bc53aab5c588d9d7b19a59fae099a74461f
Piotr Majdak <piotr@majdak.com>
2025-07-10 12:31:37 +0200

    dataset and datafile show ui improved.

--- laravel/resources/views/datafiles/show.blade.php
+++ laravel/resources/views/datafiles/show.blade.php
@@ -1,32 +1,24 @@
 <x-app-layout>
 	<x-slot name="header">
-		<x-slot name="header">
-			<x-database.header :datafile="$datafile" tabTitle="Datafile | {{ $datafile->name }}"/>
-		</x-slot>
-		<x-property name="Dataset">
-			<a href="{{ route('datasets.show', $datafile->dataset->id) }}">{{ $datafile->dataset->name }}</a><br>
-		</x-property>
-		<x-property name="Datafile name">{{ $datafile->name }}</x-property>
-		<x-property name="Datafiletype">{{ $datafile->datasetdef->datafiletype->name }}<br></x-property>
+		<x-database.header :datafile="$datafile" tabTitle="Datafile | {{ $datafile->name }}"/>
+	</x-slot>
+		
+	<x-property name="Dataset Name">
+		<a href="{{ route('datasets.show', $datafile->dataset->id) }}">{{ $datafile->dataset->name }}</a><br>
+	</x-property>
+	<x-property name="Datafile Type">{{ $datafile->datasetdef->datafiletype->name }}<br></x-property>
+	<x-property name="Datafile Name">
+		<a href="{{  asset($datafile->url()) }}">{{ $datafile->name }}</a>
+	</x-property>
 
-		@if($datafile->radar_id)
-			@hasrole('admin')
-				<x-property name="RADAR id">{{ $datafile->radar_id }}</x-property>
-			@endhasrole
-		@endif
+	@if($datafile->radar_id)
+		@hasrole('admin')
+			<x-property name="RADAR id">{{ $datafile->radar_id }}</x-property>
+		@endhasrole
+	@endif
 
-		{{-- call view based on datafiletype --}}
-		@livewire(DatafileListener::class, ['datafile' => $datafile])
+	@livewire(DatafileListener::class, ['datafile' => $datafile])
 
 	<x-button action="{{  asset($datafile->url()) }}">Download</x-button>
-	@can('update', $datafile)
-		@if($datafile->dataset->database->radar_id != null && $datafile->radar_id == null)
-			<x-button loadingText="Uploading: please wait" method="POST" action="{{ route('datafiles.uploadtoradar', $datafile) }}">Upload to RADAR</x-button>
-		@endif
-	@endcan
-	@can('delete', $datafile)
-		@if($datafile->radar_id)
-			<x-button loadingText="Deleting: please wait" method="DELETE" action="{{ route('datafiles.deletefromradar', $datafile) }}">Delete from RADAR</x-button>
-		@endif
-	@endcan
+	
 </x-app-layout>
