d6c246f4e389c19fead00e9dcaec71a6268e5b2b
Piotr Majdak <piotr@majdak.com>
2025-07-13 13:55:21 +0200

    About: improved according to Florian's comments.

--- laravel/resources/views/pages/about.blade.php
+++ laravel/resources/views/pages/about.blade.php
@@ -9,7 +9,7 @@
 	</x-slot>
 
 	<p class="max-w-prose">
-		The SONICOM Ecosystem aims at providing an ecosystem for spatial auditory data closely linked with tools for binaural rendering and auditory modeling, reinforcing the idea of reproducible research. The stored data can be integrated in <a href="https://datathek.oeaw.ac.at">the ÖAW Datathek</a>, an electronic repository registered in re3data.org and providing a sustainable basis for further research within, outside and beyond SONICOM. The Ecosystem is open-accessible, citable via DOIs, documented, searchable by metadata via a web frontend, and provides an interface for machineable download of partial data. It enables other stakeholders such as researchers, end users, and practitioners to contribute to it in.
+		The SONICOM Ecosystem aims at providing an ecosystem for spatial auditory data closely linked with tools for binaural rendering and auditory modeling, reinforcing the idea of reproducible research. The stored data can be integrated in <a href="https://datathek.oeaw.ac.at">the ÖAW Datathek</a>, an electronic repository registered in <a href="https://www.re3data.org/repository/r3d100014539">re3data.org</a> and providing a sustainable basis for further research within, outside and beyond SONICOM. The Ecosystem is open access, citable via DOIs, documented, searchable by metadata via a web frontend, and provides an interface for machineable download of partial data. It enables other stakeholders such as researchers, end users, and practitioners to contribute to.
 	</p>
 	<br>
 	<p class="max-w-prose">
@@ -19,9 +19,9 @@
 
 	<br><hr><br>
 	
-	<h3>The Ecosystem Team:</h3>
+	<h3>The Team:</h3>
 	<p class="max-w-prose">
-		The Ecosystem is developed and maintained at the <a href="https://www.oeaw.ac.at/en/ari/">Acoustics Research Institute</a> of the <a href="https://www.oeaw.ac.at/en/">Austrian Academy of Sciences</a>:
+		The SONICOM Ecosystem is developed and maintained at the <a href="https://www.oeaw.ac.at/en/ari/">Acoustics Research Institute</a> of the <a href="https://www.oeaw.ac.at/en/">Austrian Academy of Sciences</a>:
 	</p>
 	<ul class="list-disc list-inside">
 		<li><b><a href="https://www.oeaw.ac.at/en/ari/our-team/majdak-piotr">Piotr Majdak</a>:</b> Project lead, Development, Programming
@@ -29,12 +29,12 @@
 		<li><b><a href="https://www.oeaw.ac.at/en/ari/our-team/mihocic-michael">Michael Mihocic</a>:</b> Widgets, Testing, Data management
 	</ul>
 	<p class="max-w-prose">
-		The Ecosystem uses the <a href="https://datathek.oeaw.ac.at">the ÖAW Datathek</a> as a backend. The ÖAW Datathek is maintained and supported by the <a href="https://verlag.oeaw.ac.at/en/press/contact/c-40">Austrian Academy of Sciences Press</a> with <b>Herwig Stöger</b> as the contact person.
+		The SONICOM Ecosystem uses the <a href="https://datathek.oeaw.ac.at">ÖAW Datathek</a> as a backend. The ÖAW Datathek is maintained and supported by the <a href="https://verlag.oeaw.ac.at/en/press/contact/c-40">Austrian Academy of Sciences Press</a> with <b>Herwig Stöger</b> as the contact person.
 	</p>
 	
 	<br><hr><br>
 	
-	<h3>The Ecosystem terminology:</h3>
+	<h3>Terminology:</h3>
 	<ul class="list-disc list-inside max-w-prose">
 		<li><b>Database</b>: A collection of data in a specific collation of formats, e.g., a collection of HRTF and image data, where each database record contains one HRTF and one image of the ear.
 		<li><b>Dataset</b>: One record in the database, e.g., one HRTF and one image for each of the subjects. One record may contain multiple datafiles of various datatypes. The exact composition of each 'dataset' in a database is given by the dataset definition.
@@ -47,12 +47,12 @@
 	<h3>Resources and Support:</h3>
 	<ul class="list-disc list-inside max-w-prose">
 		<li><b><a href="https://github.com/sofacoustics/Ecosystem/wiki">Wiki</a>:</b> We use Github Wiki pages to provide documentation. 
-		<li><b><a href="https://github.com/sofacoustics/Ecosystem/issues">Issues</a>:</b> Check the Github Issue pages in case of troubles. If your problem has not been reported yet, issue a ticket.
+		<li><b><a href="https://github.com/sofacoustics/Ecosystem/issues">Issues</a>:</b> Check the Github Issue pages in case of troubles. If your problem has not been reported yet, please issue a new ticket <a href="https://github.com/sofacoustics/Ecosystem/issues/new">here</a>.
 		<li><b><a href="https://github.com/sofacoustics/Ecosystem">Code</a>:</b> The Ecosystem is open source: We use the <a href="https://github.com/sofacoustics/Ecosystem/tree/live">live</a> branch for the system tested and available to the users, and the <a href="https://github.com/sofacoustics/Ecosystem/tree/dev">dev</a> branch for improvements before going live. Check our source code or fork it for your own projects. 
 	</ul>
 	<br><hr><br>
 	
-	<h3>Permissions:</h3>
+	<h3>Your Permissions:</h3>
 	<ul class="list-disc list-inside">
 		@role('admin')
 			<li>You have the 'admin' role.
@@ -62,9 +62,9 @@
 	</ul>
 	<br><hr><br>
 	
-	<h3>System information:</h3>
+	<h3>System Information:</h3>
 	<ul class="list-disc list-inside">
-		<li>SONICOM Ecosystem version: {{ config('version.string') }}
+		<li>Version: {{ config('version.string') }}
 	</ul>
 	<br><hr><br>
 	
