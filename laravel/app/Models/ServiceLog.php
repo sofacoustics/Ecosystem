68d8495f5cfde93ac2e9743ae0dac44fb9a779cf
Jonnie Stuefer <jonnie@floorspot.org>
2025-07-15 17:04:13 +0200

    Adding some useful relationships

--- laravel/app/Models/ServiceLog.php
+++ laravel/app/Models/ServiceLog.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Datafile;
+use App\Models\Service;
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ServiceLog extends Model
 {
-    //
+	public function datafile(): BelongsTo
+	{
+		return $this->belongsTo(Datafile::class);
+	}
+
+	public function service(): BelongsTo
+	{
+		return $this->belongsTo(Service::class);
+	}
 }
