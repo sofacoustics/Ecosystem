501ebb56ffe1ddcb612f711221d0453782bd1161
Piotr Majdak <piotr@majdak.com>
2025-07-18 15:12:56 +0200

    Sort datafile in a dataset by the datasetdef_id

--- laravel/app/Models/Dataset.php
+++ laravel/app/Models/Dataset.php
@@ -9,41 +9,39 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Dataset extends Model
 {
-    use HasFactory;
-    
-    protected $fillable = [
-        'id', 'name', 'description', 'database_id'
-    ];    
+	use HasFactory;
+	
+	protected $fillable = [
+			'id', 'name', 'description', 'database_id'
+	];    
 
-    protected static function boot()
-    {
-        parent::boot();
-        static::deleting(function($model) {
-            $model->datafiles->each->delete();
-        });
-    }
-    /**
-     * Get the files for a dataset
-     * 
-     * https://laravel.com/docs/10.x/eloquent-relationships#one-to-many
-     */
-    public function datafiles(): HasMany
-    {
-        return $this->hasMany(Datafile::class);
-    }
+	protected static function boot()
+	{
+		parent::boot();
+		static::deleting(function($model) {
+				$model->datafiles->each->delete();
+		});
+	}
+	/**
+	 * Get the files for a dataset
+	 * 
+	 */
+	public function datafiles(): HasMany
+	{
+		return $this->hasMany(Datafile::class)->orderBy('datasetdef_id');
+	}
 
-		//jw:note This creates the database->dataset relationship using standard column names
-    public function database(): BelongsTo
-    {
-        return $this->belongsTo(Database::class);
-    }
+	//jw:note This creates the database->dataset relationship using standard column names
+	public function database(): BelongsTo
+	{
+		return $this->belongsTo(Database::class);
+	}
 
-
-    public function missing_datafiles()
-    {
-        $requiredDatasetdefs = $this->database->datasetdefs()->pluck('id');
-        $existingDatasetdefs = $this->datafiles->pluck('datasetdef_id');
-        $missingDatasetdefs = Datasetdef::whereIn('id', $requiredDatasetdefs->diff($existingDatasetdefs))->get();
-        return $missingDatasetdefs;
-    }
+	public function missing_datafiles()
+	{
+		$requiredDatasetdefs = $this->database->datasetdefs()->pluck('id');
+		$existingDatasetdefs = $this->datafiles->pluck('datasetdef_id');
+		$missingDatasetdefs = Datasetdef::whereIn('id', $requiredDatasetdefs->diff($existingDatasetdefs))->get();
+		return $missingDatasetdefs;
+	}
 }
