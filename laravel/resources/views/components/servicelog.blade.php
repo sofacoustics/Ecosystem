9c5f2afd51f1280f89ddfb2acd18576207a237be
Jonnie Stuefer <jonnie@floorspot.org>
2025-07-19 17:29:32 +0200

    Improved service log output for admins

--- laravel/resources/views/components/servicelog.blade.php
+++ laravel/resources/views/components/servicelog.blade.php
@@ -1,5 +1,6 @@
 @hasrole('admin')
 <div class="{{ $adminInfo }}">
+	<p>Service Info for Admins!</p>
 	@if($log)
 		{{-- if services_log date is less than datafile modified date, then don't show it --}}
 		@if($log->updated_at < $log->datafile->updated_at)
@@ -8,7 +9,14 @@
 			<p>Service: {{ $log->name }} (ID: {{ $log->service_id }})</p>
 			<p>Datafile: {{ $log->datafile->name }} (ID: {{ $log->datafile_id }})</p>
 			@if($log->exit_code != 0)
-				<x-alert>Last service job exited with the code {{ $log->exit_code }} ({{ $log->exit_code_text }})</x-alert>
+				<x-alert>Last service job exited with the code {{ $log->exit_code }} ({{ $log->exit_code_text }})
+					<div><b>stdout</b>:<br>
+						{{ $log->stdout }}
+					</div>
+					<div><b>stderr</b>:<br>
+						{{ $log->stderr }}
+					</div>
+				</x-alert>
 			@else
 				<p>Last service job exited with the code {{ $log->exit_code }} ({{ $log->exit_code_text }})</p>
 			@endif
