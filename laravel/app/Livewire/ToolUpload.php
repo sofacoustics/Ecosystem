6762512b9f5a9d23a7e8773b49b90eba07ca0fd0
Jonnie Stuefer <jonnie@floorspot.org>
2025-07-28 10:12:25 +0200

    Tidying up/improving log messages

--- laravel/app/Livewire/ToolUpload.php
+++ laravel/app/Livewire/ToolUpload.php
@@ -23,38 +23,35 @@ class ToolUpload extends Component
 
 	public function mount()
 	{
-		\Log::info("Tool::mount()");
 	}
-
 
 	#[On('livewire-upload-cancel')]
 	public function handleCancelUpload()
 	{
-			// Handle the cancellation
-		\Log::info("ToolUpload::handleCancelUpload()");
+		// Handle the cancellation
+		app('log')->debug("ToolUpload::handleCancelUpload()", ['feature' => 'livewire-tool-upload', 'tool_id' => $this->tool->id]);
 		//dd('handleCancelUpload()');
-			// For example, reset any related properties or perform cleanup
+		// For example, reset any related properties or perform cleanup
 	}
 
 	public function updatedFile()
 	{
-		//dd(true);
-		\Log::info("updatedFile()");
-		try 
+		app('log')->debug('updatedFile()', ['feature' => 'livewire-tool-upload', 'tool_id' => $this->tool->id]);
+		try
 		{
 			$this->save();
 		} 
-		catch (\Illuminate\Validation\ValidationException $e) 
+		catch (\Illuminate\Validation\ValidationException $e)
 		{
-			\Log::info("validation failed");
+			app('log')->debug('validation failed', ['feature' => 'livewire-tool-upload', 'tool_id' => $this->tool->id, 'exception' => $e->getMessage()]);
 			//dd($e);
-			\Log::info("deleting file");
+			app('log')->debug('deleting file', ['feature' => 'livewire-tool-upload', 'tool_id' => $this->tool->id]);
 			$this->file->delete(); // deletes the temporary file
-			\Log::info("resetting 'file'");
+			app('log')->debug('resetting \'file\'', ['feature' => 'livewire-tool-upload', 'tool_id' => $this->tool->id]);
 			$this->reset('file');
-			\Log::info("Setting 'file' to null");
+			app('log')->debug('Setting \'file\' to null', ['feature' => 'livewire-tool-upload', 'tool_id' => $this->tool->id]);
 			$this->file = null;
-			\Log::info("Setting custom validation error message");
+			app('log')->debug('Setting custom validation error message', ['feature' => 'livewire-tool-upload', 'tool_id' => $this->tool->id]);
 			$this->addError('file', $e->getMessage()); // error messages weren't appearing without this, when using try/catch!
 		}
 	}
@@ -68,7 +65,7 @@ class ToolUpload extends Component
 	{
 		if($this->tool->radar_status > 1)
 		{
-			app('log')->error("ToolUpload::save() - attempting to save a new file when persistent publication has already been requested!");
+			app('log')->error("ToolUpload::save() - attempting to save a new file when persistent publication has already been requested!", ['feature' => 'livewire-tool-upload', 'tool_id' => $this->tool->id]);
 			if($this->tool->radar_status == 2)
 				$message = "Uploading a new file is not permitted since persistent publication for this tool has been requested!";
 			else if($this->tool->radar_status == 3)
@@ -81,10 +78,10 @@ class ToolUpload extends Component
 		}
 		if(!$this->file)
 		{
-			app('log')->debug("ToolUpload::save() - $this->file is empty, so returning early!");
+			app('log')->debug('ToolUpload::save() - $this->file is empty, so returning early!', ['feature' => 'livewire-tool-upload', 'tool_id' => $this->tool->id]);
 			return;
 		}
-		app('log')->debug("ToolUpload::save()");
+		app('log')->debug('ToolUpload::save()', ['feature' => 'livewire-tool-upload', 'tool_id' => $this->tool->id]);
 
 		$this->authorize('update', $this->tool); // check if tool can be modified
 		$this->tool->removefile(); // if there was already a file --> remove the old one
