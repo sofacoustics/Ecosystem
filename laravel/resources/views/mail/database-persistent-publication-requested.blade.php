bbbade5419c7b81df26819def43c0f34a8906bc6
Jonnie Stuefer <jonnie@floorspot.org>
2025-07-31 21:00:25 +0200

    Improving email message

--- laravel/resources/views/mail/database-persistent-publication-requested.blade.php
+++ laravel/resources/views/mail/database-persistent-publication-requested.blade.php
@@ -1,5 +1,5 @@
 <div>
 	<p>Dear {{ $database->user->name }},</p>
-	<p>Your request for the persistent publication of the database <a href="{{ route('databases.show', $database->id) }}">{{ $database->title }}</a> has been send to the SONICOM Ecosystem admins. You will informed when the publication has been approved.</p>
+	<p>Your publication upload has finished and your request for the persistent publication of the database <a href="{{ route('databases.show', $database->id) }}">{{ $database->title }}</a> has been send to the SONICOM Ecosystem admins. You will informed when the publication has been approved.</p>
 	<x-mail.footer />
 </div>
