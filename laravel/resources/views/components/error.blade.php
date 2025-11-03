eb70a08da6c270c2de06912fb44c4d88c2ecc1f3
Jonnie Stuefer <jonnie@floorspot.org>
2025-11-03 05:57:21 +0100

    error component can now be used for displaying general error too

--- laravel/resources/views/components/error.blade.php
+++ laravel/resources/views/components/error.blade.php
@@ -1,12 +1,22 @@
 {{--
-    A component to display an error message
+	A component to display an error message
 
-    Parameters:
+	Parameters:
 
-        attribute`  The attribute to display the error message for
+		attribute`	The attribute to display the error message for
+
+	Notes:
+
+		J.W. 3.11.2025	
+
+		The code where not $attribute is set works. I'm not sure that the $attribute code works though!
 --}}
 <div>
-        @error($attribute)
-            <span class="text-red-500">{{ $message }}</span>
-        @enderror
+	@if(isset($attribute))
+		@error($attribute)
+			<span class="text-red-500">{{ $message }}</span>
+		@enderror
+	@else
+			<span class="text-red-500">{{ $slot }}</span>
+	@endif
 </div>
