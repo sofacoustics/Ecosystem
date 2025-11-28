47244813ae5822e5035a1b70017c9622c1b24d58
Jonnie Stuefer <jonnie@floorspot.org>
2025-11-28 14:00:56 +0100

    Service log is now created when service is started with the exit_code -666. This is then overwritten, when job finished or times out. If it's still there, then it means that supervisorctl killed the job.

--- laravel/app/Jobs/Service.php
+++ laravel/app/Jobs/Service.php
@@ -76,21 +76,10 @@ class Service implements ShouldQueue
 		$datafile_id = $this->datafile->id;
 		$directory=storage_path('app/services/' . $this->service->id);
 		$log = "  widget=$widget_id, service=$service_id, datafile=$datafile_id, directory=$directory\n";
-		#$command = 'bash -c "setsid bash -c \'' .	$this->service->exe . ' ' . $this->service->parameters . ' \"' . $this->datafile->absolutepath() . '\"' . '\'"';
-		#$command = 'bash -c "setsid bash -c \'' .	$this->service->exe . ' ' . $this->service->parameters . ' \"' . $this->datafile->absolutepath() . '\"' . '\'"';
-		//$command = 'bash -c "setsid bash -c \'sleep 20 && echo \"Child finished.\" >> /tmp/child_output.txt\'"';
 		$command = $this->service->exe . ' ' . $this->service->parameters . ' "' . $this->datafile->absolutepath() . '"';
 		$log .= ' command='.$command."\n";
-		//$command = 'exec setsid ' . $this->service->exe . ' ' . $this->service->parameters . ' "' . $this->datafile->absolutepath() . '"';
 
 		$start = microtime(true);
-
-		/*
-		$process = Process::timeout($timeout)
-			->path($directory)
-			//->setPty(true) // enable process group termination
-			->start($command); // run with output
-		 */
 
 		// parse environmental variables and command arguments
 		$pairs = explode(' ', $this->service->exe);
@@ -106,22 +95,13 @@ class Service implements ShouldQueue
 		$parameters = explode(' ', $this->service->parameters);
 		foreach($parameters as $param)
 			$args[] = $param;
-		//$args = array_merge($args, explode(' ', $this->service->parameters));
 		$args[] = $this->datafile->absolutepath();
 		foreach($envvars as $k => $v)
 			$log .= "  environmental variable: $k => $v\n";
 		foreach($args as $arg)
 			$log .= "  command argument: $arg\n";
-		//Log::info('The command arguments', $args);
 
-
-		//$process = new Process([$this->service->exe, $this->service->parameters, $this->datafile->absolutepath()], null,[
 		$process = new Process($args, null,$envvars);
-			/*
-		$process = new Process(['xvfb-run', '-a', 'octave-cli', 'BRIRGeometry.m', $this->datafile->absolutepath()], null,[
-			'XDG_CACHE_HOME' => '/run/user/33/sonicom-xdg-cache-home',
-			'XDG_RUNTIME_DIR' => '/run/user/33',
-		]);*/
 		$process->setWorkingDirectory($directory);
 		$process->setTimeout($this->service->timeout); // use class timeout (set from service column)
 
@@ -131,7 +111,21 @@ class Service implements ShouldQueue
 
 		$jobid = $this->job?->getJobId();
 
-		app('log')->channel('services_stack')->notice("Job $jobid Process $pid has started:", ['data' => $args]);
+		// add to service log table
+		$serviceLog = new ServiceLog();
+		$serviceLog->service_id = $this->service->id;
+		$serviceLog->datafile_id  = $this->datafile->id;
+		$serviceLog->exit_code	= -666;
+		$serviceLog->exit_code_text = "Job has been started. If this text remains here, then the process was killed by supervisorctl or itself and Houston - we have a problem!";
+		$serviceLog->execution_time  = microtime(true) - $start;
+		$serviceLog->name  = $this->service->name;
+		$serviceLog->description  = $this->service->description;
+		$serviceLog->exe  = $this->service->exe;
+		$serviceLog->parameters  = $this->service->parameters;
+		$serviceLog->save();
+
+		app('log')->channel('services_stack')->notice("Job $jobid Process $pid has started (timeout: $this->timeout):", ['data' => $args]);
+		app('log')->channel('services_stack')->debug("Job $jobid Process $pid has started (timeout: $this->timeout): $log");
 		try {
 			// Periodically check for timeout while the process is running
 			while ($process->isRunning()) {
@@ -139,6 +133,11 @@ class Service implements ShouldQueue
 				$findChildren = new Process(['pgrep', '-P', $pid]);
 				$findChildren->run();
 				$childPids = array_filter(explode("\n", $findChildren->getOutput()));
+				app('log')->channel('services_stack')->debug("Job $jobid Process $pid is running");
+				$serviceLog->execution_time  = microtime(true) - $start;
+				$serviceLog->stdout = $process->getOutput();
+				$serviceLog->stderr = $process->getErrorOutput();
+				$serviceLog->save();
 				$process->checkTimeout(); // This will throw if timeout is reached
 				usleep(100000); // Sleep for 0.1 seconds
 			}
@@ -146,7 +145,9 @@ class Service implements ShouldQueue
 			$errorOutput = $process->getErrorOutput();
 			app('log')->channel('services_stack')->debug("Process $pid has finished");
 		} catch (ProcessTimedOutException $e) {
+		    app('log')->channel('services_stack')->debug("Job $jobid Process $pid has reached it's timeout");
 			app('log')->channel('services_stack')->warning("Process $pid has reached it's timeout");
+		    app('log')->channel('services_stack')->debug("Job $jobid Process $pid: sending children SIGKILL");
 			// Kill child processes
 			foreach ($childPids as $childPid) {
 				if (is_numeric($childPid)) {
@@ -154,76 +155,48 @@ class Service implements ShouldQueue
 					posix_kill((int)$childPid, SIGKILL);
 				}
 			}
-			//jw:todo Send email
+		} catch (\Exception $e) {
+			app('log')->channel('services_stack')->warning("Process $pid - generic error");
+		} finally {
+			app('log')->channel('services_stack')->debug("Process $pid - entering 'finally' section");
+			$duration = microtime(true) - $start;
+			$execution = "execution time: $duration";
+			$exitCode = $process->getExitCode();
+			$exitCodeText = $process->getExitCodeText();
 
-			//$process->signal(SIGKILL);
-			//$log .= " Killing via SIGKILL\n";
-		}
+			$datafilelogfile = $this->datafile->directory() . '/service-' . $this->service->id . '-PID-' . $pid . '.stdout';
+			$datafileerrorfile = $this->datafile->directory() . '/service-' . $this->service->id . '-PID-' . $pid . '.stderr';
+			$log .= "  logging output to file " . Storage::disk('sonicom-data')->path($datafilelogfile) . "\n";
+			$log .= "  logging errors to file " . Storage::disk('sonicom-data')->path($datafileerrorfile) . "\n";
 
-		$duration = microtime(true) - $start;
-		$execution = "execution time: $duration";
-		$exitCode = $process->getExitCode();
-		$exitCodeText = $process->getExitCodeText();
-
-		$datafilelogfile = $this->datafile->directory() . '/service-' . $this->service->id . '-PID-' . $pid . '.stdout';
-		$datafileerrorfile = $this->datafile->directory() . '/service-' . $this->service->id . '-PID-' . $pid . '.stderr';
-		$log .= "  logging output to file " . Storage::disk('sonicom-data')->path($datafilelogfile) . "\n";
-		$log .= "  logging errors to file " . Storage::disk('sonicom-data')->path($datafileerrorfile) . "\n";
-
-/*
-		// wait for completion with timeout handling
-		$successful = false;
-		try {
-			$result = $process->wait();
-			$exitCode = $result->exitCode();
-			$successful = $result->successful();
-			$output = $result->output();
-			$errorOutput = $result->errorOutput();
-			// write output to a service log file in the datafile directory
-		} catch (ProcessTimedOutException $e) {
-			// Handle timeout
-			//$process->signal(SIGKILL); // Force kill if needed
-			$process->stop();
-			$exitCode = -99; // timeout
-			$output = "";
-			$errorOutput = "$timeout second timeout reached";
-			$log .= "  exception: $timeout second timeout reached!\n";
+			$duration = microtime(true) - $start;
+			$this->datafile->last_service_error_code = $exitCode;
+			$this->datafile->save();
+			// add to service log table
+			$serviceLog->exit_code	= $exitCode;
+			$serviceLog->exit_code_text = $exitCodeText;
+			$serviceLog->execution_time  = $duration;
+			if(isset($output))
+			{
+				$serviceLog->stdout  = $output;
+				Storage::disk('sonicom-data')->put($datafilelogfile, $output);
+			}
+			if(isset($errorOutput))
+			{
+				$serviceLog->stderr  = $errorOutput;
+				Storage::disk('sonicom-data')->put($datafileerrorfile, $errorOutput);
+			}
+			$serviceLog->save();
+			$log .= "  service timeout value: " . $this->timeout . " process timeout value: " . $this->service->timeout . "\n";
+			if($process->isSuccessful())
+				$log .= "  process finished successfully after $duration seconds (exitCode: " . $exitCode . ")\n";
+			else
+				$log .= "  process failed after $duration seconds (exitCode: " . $exitCode . ")\n";
+			DatafileProcessed::dispatch($this->datafile->id);
+			if($exitCode!=0)
+				app('log')->channel('services_stack')->warning("Process $pid info:\n$log"); // log all in one go so it doesn't get interspersed with other log messages
+			else
+				app('log')->channel('services_stack')->notice("Process $pid info:\n$log"); // log all in one go so it doesn't get interspersed with other log messages
 		}
-*/
-		$duration = microtime(true) - $start;
-		$this->datafile->last_service_error_code = $exitCode;
-		$this->datafile->save();
-		// add to service log table
-		$serviceLog = new ServiceLog();
-		$serviceLog->service_id = $this->service->id;
-		$serviceLog->datafile_id  = $this->datafile->id;
-		$serviceLog->exit_code	= $exitCode;
-		$serviceLog->exit_code_text = $exitCodeText;
-		$serviceLog->execution_time  = $duration;
-		$serviceLog->name  = $this->service->name;
-		$serviceLog->description  = $this->service->description;
-		$serviceLog->exe  = $this->service->exe;
-		$serviceLog->parameters  = $this->service->parameters;
-		if(isset($output))
-		{
-			$serviceLog->stdout  = $output;
-			Storage::disk('sonicom-data')->put($datafilelogfile, $output);
-		}
-		if(isset($errorOutput))
-		{
-			$serviceLog->stderr  = $errorOutput;
-			Storage::disk('sonicom-data')->put($datafileerrorfile, $errorOutput);
-		}
-		$serviceLog->save();
-		$log .= "  service timeout value: " . $this->timeout . " process timeout value: " . $this->service->timeout . "\n";
-		if($process->isSuccessful())
-			$log .= "  process finished successfully after $duration seconds (exitCode: " . $exitCode . ")\n";
-		else
-			$log .= "  process failed after $duration seconds (exitCode: " . $exitCode . ")\n";
-		DatafileProcessed::dispatch($this->datafile->id);
-		if($exitCode!=0)
-			app('log')->channel('services_stack')->warning("Process $pid info:\n$log"); // log all in one go so it doesn't get interspersed with other log messages
-		else
-			app('log')->channel('services_stack')->notice("Process $pid info:\n$log"); // log all in one go so it doesn't get interspersed with other log messages
 	}
 }
