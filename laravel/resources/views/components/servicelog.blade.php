bfaebbc4beb534bf24545fa5ee881f4c32f44f92
Jonnie Stuefer <jonnie@floorspot.org>
2025-12-01 13:29:57 +0100

    Adding created and updated dates to service log component output

--- laravel/resources/views/components/servicelog.blade.php
+++ laravel/resources/views/components/servicelog.blade.php
@@ -8,6 +8,7 @@
 		@else
 			<p>Service: {{ $log->name }} (ID: {{ $log->service_id }})</p>
 			<p>Datafile: {{ $log->datafile->name }} (ID: {{ $log->datafile_id }})</p>
+			<p>Created: {{ $log->created_at }} Updated: {{ $log->updated_at }}</p>
 			@if($log->exit_code != 0)
 				<x-alert>Last service job exited with the code {{ $log->exit_code }} ({{ $log->exit_code_text }})
 					<div><b>stdout</b>:<br>
