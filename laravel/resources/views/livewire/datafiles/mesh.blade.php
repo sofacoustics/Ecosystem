bede4c22069ec6a064ccdfbf9c685301aac6f848
Piotr Majdak <piotr.majdak@oeaw.ac.at>
2025-12-27 17:21:30 +0100

    Mesh Widget: progress bar added to show the progress of downloading when viewing.

--- laravel/resources/views/livewire/datafiles/mesh.blade.php
+++ laravel/resources/views/livewire/datafiles/mesh.blade.php
@@ -2,20 +2,37 @@
 	<p><x-datafiles-properties :fileSizeInBytes="$fileSizeInBytes" :createdAt="$created_at" :updatedAt="$updated_at"/></p>
 
 	<script src="/js/stl_viewer/stl_viewer.min.js"></script>
+
+  <progress id="progress{{ $datafile->id }}" value="0" max="1" class="w-full bg-gray-200 h-4 rounded-full transition-all duration-500"></progress>
 	
 	<div id="stl_cont{{ $datafile->id }}" style="width:500px;height:500px;margin:0 auto; text-align:left;"></div>
 
 	<script>
+	
+		function stl_progress{{ $datafile->id }}(load_status, load_session)
+		{
+			Object.keys(load_status).forEach(function(model_id) //go over all models to be loaded
+			{
+				if (load_status[model_id].load_session==load_session) //need to make sure we're on the last loading session (not counting previous loaded models)
+				{				
+					bar = document.getElementById("progress{{ $datafile->id }}");
+					if (load_status[model_id].loaded < load_status[model_id].total)
+						bar.value = load_status[model_id].loaded / load_status[model_id].total;
+					else
+					  bar.style.display = 'none';
+				}
+			});
+		}    
+		
 		var stl_viewer{{ $datafile->id }}=new StlViewer
 		(
 			document.getElementById("stl_cont{{ $datafile->id }}"),
 			{
-				models:
-				[
-					{filename:"{{ $datafile->asset('',true) }}"}
-				]
+				loading_progress_callback:stl_progress{{ $datafile->id }},
+				models: [{filename:"{{ $datafile->asset('',true) }}"}],
 			}
 		);
+				
 	</script>
 
 </div>
