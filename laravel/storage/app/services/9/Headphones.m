5584514ce340e9b35456065bd7a0e53f2275f6ec
Michael Mihocic <michael.mihocic@oeaw.ac.at>
2026-06-18 09:27:12 +0200

    Headphones.m service file: - bug fixed when plotting more than 6 curves per side (implementing mod code to repeat colors) - y-limit range fixed for high amplitudes

--- laravel/storage/app/services/9/Headphones.m
+++ laravel/storage/app/services/9/Headphones.m
@@ -2,6 +2,7 @@
 
 % #Author: Michael Mihocic: support of SimpleHeadphoneIR (16.04.2025)
 % #Author: Michael Mihocic: create csv files with properties (09.07.2025)
+% #Author: Michael Mihocic: bug fixed when plotting more than 6 curves per side (implementing mod code to repeat colors); y-limit range fixed for high amplitudes (18.06.2026)
 %
 % Copyright (C) Acoustics Research Institute - Austrian Academy of Sciences
 % Licensed under the EUPL, Version 1.2 or - as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "License")
@@ -12,99 +13,103 @@
 
 function Headphones(SOFAfile)
 
-	isoctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
-	addpath('../shared'); % add the path to shared functions
-	logfile="Headphones.log";
-	fid = fopen(logfile, "w");
-	s = pwd;
-	disp(["pwd = " s]);
+isoctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
+addpath('../shared'); % add the path to shared functions
+logfile="Headphones.log";
+fid = fopen(logfile, "w");
+s = pwd;
+disp(["pwd = " s]);
 
-	%% Prologue: (un)comment here if you want to:
-	close all; % clean-up first
-	tic; % timer
-	SOFAstart; % remove this optionally
-	warning('off'); %jw:note disable all warnings
+%% Prologue: (un)comment here if you want to:
+close all; % clean-up first
+tic; % timer
+SOFAstart; % remove this optionally
+warning('off'); %jw:note disable all warnings
 
-	%jw:note Check if function called with parameter. If not, use command line parameter^M
-	if(exist("SOFAfile"))
-		if(length(SOFAfile)==0)
-			disp('The SOFA file name SOFAfile is empty');
-		end
-	else
-		% Use command line parameter for SOFAfile
-		disp(argv);
-		arg_list = argv();
-		fn = arg_list{1};
-		disp(fn);
-		SOFAfile = fn;
-	end
-	%disp(["SOFAfile = " SOFAfile]);
+%jw:note Check if function called with parameter. If not, use command line parameter^M
+if(exist("SOFAfile"))
+  if(length(SOFAfile)==0)
+    disp('The SOFA file name SOFAfile is empty');
+  end
+else
+  % Use command line parameter for SOFAfile
+  disp(argv);
+  arg_list = argv();
+  fn = arg_list{1};
+  disp(fn);
+  SOFAfile = fn;
+end
+%disp(["SOFAfile = " SOFAfile]);
 
-	%% Load SOFA file
-	Obj=SOFAload(SOFAfile);
+%% Load SOFA file
+Obj=SOFAload(SOFAfile);
 
-	SaveSOFAproperties(Obj, SOFAfile);
-	if isoctave; fputs(fid, ["Successfully saved SOFA details to csv files\n"]); end
+SaveSOFAproperties(Obj, SOFAfile);
+if isoctave; fputs(fid, ["Successfully saved SOFA details to csv files\n"]); end
 
-	if isoctave; fputs(fid, [ "About to plot\n"]); end
+if isoctave; fputs(fid, [ "About to plot\n"]); end
 
-	%% Plot a few figures
-	switch Obj.GLOBAL_SOFAConventions
-		% maybe other directivity cases will follow
-		case 'SimpleHeadphoneIR';
-			if isoctave; fputs(fid, [ "case Headphones\n"]); end
-			figure('Name',SOFAfile);
-			if isoctave; fputs(fid, [ "just done figure\n"]); end
-					 
-			hold on; box on;
-			cols='bgrmky';
+%% Plot a few figures
+switch Obj.GLOBAL_SOFAConventions
+  % maybe other directivity cases will follow
+  case 'SimpleHeadphoneIR';
+    if isoctave; fputs(fid, [ "case Headphones\n"]); end
+    figure('Name',SOFAfile);
+    if isoctave; fputs(fid, [ "just done figure\n"]); end
 
-			%if ~isoctave
-			if ~isoctave
-				if isfield(Obj, 'MeasurementDate')
-						meastime=[0; diff(Obj.MeasurementDate)]; % diff not working in Octave
-				else
-						meastime=diff(Obj.GLOBAL_DateCreated); % diff not working in Octave
-				end
-			end
+    hold on; box on;
+    cols='bgrmky';
 
-			for ii=1:Obj.API.M
-				plot(20*log10(abs(fft(squeeze(Obj.Data.IR(ii,1,:)),Obj.Data.SamplingRate))),cols(ii));
-				leg{ii}=['Left #' num2str(ii)]; 
-			end
+    %if ~isoctave
+    if ~isoctave
+      if isfield(Obj, 'MeasurementDate')
+        meastime=[0; diff(Obj.MeasurementDate)]; % diff not working in Octave
+      else
+        meastime=diff(Obj.GLOBAL_DateCreated); % diff not working in Octave
+      end
+    end
 
-			for ii=1:Obj.API.M
-				plot(20*log10(abs(fft(squeeze(Obj.Data.IR(ii,2,:)),Obj.Data.SamplingRate)))-20,cols(ii));  
-					% if ~isoctave
-					%   leg{ii+Obj.API.M}=['Right #' num2str(ii) ':' num2str(meastime(ii)) ' seconds later']; 
-					% else
-						leg{ii+Obj.API.M}=['Right #' num2str(ii)]; 
-					% end
-			end
+    for ii=1:Obj.API.M
+      % plot(20*log10(abs(fft(squeeze(Obj.Data.IR(ii,1,:)),Obj.Data.SamplingRate))));
+      plot(20*log10(abs(fft(squeeze(Obj.Data.IR(ii,1,:)),Obj.Data.SamplingRate))),cols(mod(ii, length(cols))+1) );
+      % plot(20*log10(abs(fft(squeeze(Obj.Data.IR(ii,1,:)),Obj.Data.SamplingRate))),cols(ii));
+      leg{ii}=['Left #' num2str(ii)];
+    end
 
-			xlim([-200 18200]);
+    for ii=1:Obj.API.M
+      % plot(20*log10(abs(fft(squeeze(Obj.Data.IR(ii,2,:)),Obj.Data.SamplingRate)))-20);
+      plot(20*log10(abs(fft(squeeze(Obj.Data.IR(ii,2,:)),Obj.Data.SamplingRate)))-20,cols(mod(ii, length(cols))+1) );
+      % plot(20*log10(abs(fft(squeeze(Obj.Data.IR(ii,2,:)),Obj.Data.SamplingRate)))-20,cols(ii));
+      % if ~isoctave
+      %   leg{ii+Obj.API.M}=['Right #' num2str(ii) ':' num2str(meastime(ii)) ' seconds later'];
+      % else
+      leg{ii+Obj.API.M}=['Right #' num2str(ii)];
+      % end
+    end
 
-			axis([-200 18200 -65 15]);
-			% leg{1}='#1, first measurement';
-			legend(leg,'Location','best');
-			title('Amplitude Spectra of Headphones Measurements: Left, Right [-20 dB]')
-			xlabel('Frequency (Hz)')
-			ylabel('Amplitude (dB)')
+    xlim([-200 18200]);
+    yMax = max(15,ceil(max(20*log10(abs(fft(squeeze(Obj.Data.IR(1,1,:)),Obj.Data.SamplingRate))))));
+    axis([-200 18200 -65 yMax]);
+    % leg{1}='#1, first measurement';
+    legend(leg,'Location','best');
+    title('Amplitude Spectra of Headphones Measurements: Left, Right [-20 dB]')
+    xlabel('Frequency (Hz)')
+    ylabel('Amplitude (dB)')
 
-			if isoctave; fputs(fid, [ "just done some figure adaptations\n"]); end
-			
-			% set(gcf, 'Name', 'SOFAfile')
-			% if isoctave; fputs(fid, [ "renamed figure\n"]); end
-			print ('-dpng', "-r600", [SOFAfile '_1.png'])
-			%print ("-r600", '/tmp/hrtf_1.png');
-			if isoctave; fputs(fid, [ "just printed " SOFAfile "_1.png\n"]); end
-	end % switch
+    if isoctave; fputs(fid, [ "just done some figure adaptations\n"]); end
+
+    % set(gcf, 'Name', 'SOFAfile')
+    % if isoctave; fputs(fid, [ "renamed figure\n"]); end
+    print ('-dpng', "-r600", [SOFAfile '_1.png'])
+    %print ("-r600", '/tmp/hrtf_1.png');
+    if isoctave; fputs(fid, [ "just printed " SOFAfile "_1.png\n"]); end
+end % switch
 
 
-	%% Epilogue: (un)comment if you want to:
-	disp('DONE');
-	if isoctave; fclose(fid); end;
-	toc; % timer
+%% Epilogue: (un)comment if you want to:
+disp('DONE');
+if isoctave; fclose(fid); end;
+toc; % timer
 
 end
 
