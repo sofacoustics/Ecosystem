sofacoustics/Ecosystem

commit 5c6407b51ca54a6e857ec05657277f8f8c3e9215

Bugfix: services no longer started when database is uploaded to RADAR. This was happening, since each 'datafile' was modified (radar_id set) and therefore triggering the service (via the datafile observer). Now using explicit app/Models/Datafile::dispatchService() function call to trigger service rather than 'touch'

laravel/routes/web.php
@@ -115,7 +115,7 @@ Route::delete('/datasets/{dataset}/deletefromradar', [DatasetController::class, 
 Route::resource('datafiles', DatafileController::class);
 Route::post('/datafiles/{datafile}/uploadtoradar', [DatafileController::class, 'uploadtoradar'])->name('datafiles.uploadtoradar');
 Route::delete('/datafiles/{datafile}/deletefromradar', [DatafileController::class, 'deletefromradar'])->name('datafiles.deletefromradar');
-Route::post('/datafiles/{datafile}/touch', [DatafileController::class, 'touch'])->name('datafiles.touch');
+Route::post('/datafiles/{datafile}/rerunservice', [DatafileController::class, 'rerunService'])->name('datafiles.rerunservice')->middleware('auth', 'role:admin');
 // DATASETDEFS
 Route::resource('datasetdefs', DatasetdefController::class);
 Route::get('datasetdefs/{datasetdef}/duplicate', [DatasetdefController::class, 'duplicate'])->name('datasetdefs.duplicate');

laravel/app/Http/Controllers/DatafileController.php
@@ -142,12 +142,11 @@ class DatafileController extends Controller
 		return redirect()->back()->with('error', 'This datafile is not associated with a RADAR file');
 	}
 
-	public function touch(Datafile $datafile)
+	public function rerunService(Datafile $datafile)
 	{
-		// update the modified data
-		$datafile->touch();
-		$datafile->save();
-
+		app('log')->debug("datafile controller: rerunService() - (datafile: $datafile->id)");
+		$datafile->dispatchService();
 		return redirect()->back();
 	}
+
 }

laravel/app/Livewire/DatabaseUpload.php
@@ -187,18 +187,17 @@ class DatabaseUpload extends Component
 			$datafile->mimetype = $file->getMimeType();
 			$datafile->save(); // save so datafile has ID (necessary for saving file)
 
-			if($existing)
-			{
-				$this->debug(1, "Touching datafile to set updated_at");
-				$datafile->touch(); // touch the file to reset 'updated_at' and trigger DatafileObserver
-			}
 			$directory = $datafile->directory();
 			$this->dispatch('saving-file', name: $datafile->name); // dispatch a browser event
 			$this->dispatch('showFlashMessage', ['type' => 'success', 'message' => 'storeAs']);
-				// Save the file to disk (=move from temporary location)
+			// Save the file to disk (=move from temporary location)
 			$file->storeAs("$directory", "$datafile->name", 'sonicom-data');
 			//jw:todo add to 'saved' and remove from '$uploads'
 			$this->saved[] = $originalName;
+
+			// now dispatch service
+			$this->debug(1, "Dispatching service from saveDatafile() for datafile $datafile->id");
+			$datafile->dispatchService();
 		}
 		// delete from livewire-tmp, but don't remove from uploads, since otherwise all hell
 		// will break loose with the index!

laravel/app/Models/Datafile.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Facades\Storage;
 
+use App\Jobs\Service;
 
 class Datafile extends Model
 {
@@ -104,5 +105,29 @@ class Datafile extends Model
 			return true;
 		return false;
 
+	}
+
+	public function dispatchService()
+	{
+		app('log')->debug("datafile: dispatchService");
+		$widget = $this->datasetdef->widget;
+		if($widget)
+		{
+			app('log')->debug("datafile: dispatchService (widget: $widget->id, datafile: $this->id)");
+			$service = $widget->service;
+			if($service)
+			{
+				//jw:note If you want to debug a job using vscode, you *must* use the 'sync' queue, not the 'database' queue
+				Service::dispatch($widget, $this);
+			}
+		}
+		else
+		{
+			app('log')->warning("datafile: dispatchService - NO WIDGET defined", [
+				'feature' => 'widgets',
+				'datafile_id' => $this->id,
+				'datasetdef_id' => $this->datasetdef->id
+			]);
+		}
 	}
 }

laravel/app/Observers/DatafileObserver.php
@@ -18,7 +18,8 @@ class DatafileObserver
     {
         //
         app('log')->debug("DatafileObserver::created");
-        $this->dispatchService($datafile);
+				//jw:note We now have an explicit function to start the service. This gives us more
+				//        control about when it is called
     }
 
     /**
@@ -27,7 +28,9 @@ class DatafileObserver
     public function updated(Datafile $datafile): void
     {
         app('log')->debug('DatafileObserver::updated()');
-        $this->dispatchService($datafile);
+				//jw:note we're no longer running a service when datafile attributes change, since they may be
+				//        attributes that require no re-rendering. Running the service can, however, be called
+				//        explicity via the Datafile::dispatchService() function. 
     }
 
     /**
@@ -79,26 +82,4 @@ class DatafileObserver
 	// private
 	////////////////////////////////////////////////////////////////////////////////
 
-    private function dispatchService(Datafile $datafile)
-    {
-        $widget = $datafile->datasetdef->widget;
-        if($widget)
-        {
-            app('log')->debug("dispatchService (widget: $widget->id, datafile: $datafile->id)");
-            $service = $widget->service;
-            if($service)
-            {
-				//jw:note If you want to debug a job using vscode, you *must* use the 'sync' queue, not the 'database' queue
-                Service::dispatch($widget, $datafile);
-            }
-        }
-		else
-		{
-			app('log')->warning("dispatchService - NO WIDGET defined", [
-				'feature' => 'widgets',
-				'datafile_id' => $datafile->id,
-				'datasetdef_id' => $datafile->datasetdef->id
-			]);
-		}
-    }
 }

laravel/resources/views/datafiles/show.blade.php
@@ -15,7 +15,7 @@
 		<x-button method="DELETE" class="inline" action="{{ route('datafiles.destroy', [$datafile]) }}">Delete</x-button>
 	@endcan
 	@hasrole('admin')
-		<x-button method="POST" class="inline" action="{{ route('datafiles.touch', [$datafile]) }}">Rerun service</x-button>
+		<x-button method="POST" class="inline" action="{{ route('datafiles.rerunservice', [$datafile]) }}">Rerun service</x-button>
 	@endhasrole
 
 	<x-property name="Widget Name">

laravel/resources/views/datasets/show.blade.php
@@ -17,7 +17,7 @@
 			<x-button method="DELETE" class="inline" action="{{ route('datafiles.destroy', [$datafile]) }}">Delete</x-button>
 		@endcan
 		@hasrole('admin')
-			<x-button method="POST" class="inline" action="{{ route('datafiles.touch', [$datafile]) }}">Rerun service</x-button>
+			<x-button method="POST" class="inline" action="{{ route('datafiles.rerunservice', [$datafile]) }}">Rerun service</x-button>
 		@endhasrole
 		<div class="ml-2" wire:key="{{ $datafile->id }}">
 			<x-property name="Datafile Type">

commit 9599990b60887b35740add06e146ba8f9bd5989b

Bugfix: Datasetdef 'rerunService()' now triggering service (bug introduced in 5c6407b)

laravel/app/Models/Datasetdef.php
@@ -36,10 +36,8 @@ class Datasetdef extends Model
 		app('log')->info("rerunService for datasetdef $this->id");
 		foreach($this->datafiles as $datafile)
 		{
-			// touch datafile to cause service to rerun
 			app('log')->info("rerunService for datafile $datafile->id");
-			$datafile->touch();
-			$datafile->save();
+			$datafile->dispatchService();
 		}
 	}
 }

laravel/resources/views/databases/datasetdefs/index.blade.php
@@ -71,7 +71,7 @@
 				<x-td>{{ $datasetdef->description }}</x-td>
 				@role('admin') 
 					<x-td>{{ $datasetdef->datafiletype->id }}</x-td>
-					<x-td><x-button method="GET" action="{{ route('datasetdefs.rerun', [$datasetdef]) }}" class="inline">Rerun service</x-button></x-td>
+					<x-td><x-button method="POST" action="{{ route('datasetdefs.rerunservice', [$datasetdef]) }}" class="inline">Rerun service</x-button></x-td>
 				@endrole
 			</tr>
 		@endforeach

laravel/routes/web.php
@@ -120,7 +120,7 @@ Route::post('/datafiles/{datafile}/rerunservice', [DatafileController::class, 'r
 // DATASETDEFS
 Route::resource('datasetdefs', DatasetdefController::class);
 Route::get('datasetdefs/{datasetdef}/duplicate', [DatasetdefController::class, 'duplicate'])->name('datasetdefs.duplicate');
-Route::get('datasetdefs/{datasetdef}/rerun', [DatasetdefController::class, 'rerunService'])->name('datasetdefs.rerun')->middleware('auth', 'role:admin');
+Route::post('datasetdefs/{datasetdef}/rerunservice', [DatasetdefController::class, 'rerunService'])->name('datasetdefs.rerunservice')->middleware('auth', 'role:admin');
 
 // CREATORS
 Route::resource('creators', CreatorController::class);