95de8d61f0606e703fa5199a92be3d18f4ec78ca
Jonnie Stuefer <jonnie@floorspot.org>
2026-08-28 13:19:31 +0200

    Bugfix: displaying livewire service log component for scheduled job no longer throwing exception

--- laravel/resources/views/components/servicelog.blade.php
+++ laravel/resources/views/components/servicelog.blade.php
@@ -1,6 +1,5 @@
 @hasrole('admin')
 <div class="{{ $adminInfo }}">
-	<p>Service Info for Admins!</p>
-		<livewire:service-log-output :servicelog="$log"/>
-</div>
+  <livewire:service-log-output :servicelog="$log"/>
+  </div>
 @endhasrole
