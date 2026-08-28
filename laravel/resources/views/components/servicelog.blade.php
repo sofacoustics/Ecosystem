544965216a665d41d6784e26e6b6a02b94024aa4
Jonnie Stuefer <jonnie@floorspot.org>
2026-08-28 15:58:51 +0200

    Revert "Service log info now a livewire component. Still just for admins. Updates every second, so long as it's visible"
    This reverts commit fc8b0dc2cc702c9adbe20818f7a7ce73e05cdf1a.

--- laravel/resources/views/components/servicelog.blade.php
+++ laravel/resources/views/components/servicelog.blade.php
@@ -1,6 +1,29 @@
 @hasrole('admin')
 <div class="{{ $adminInfo }}">
 	<p>Service Info for Admins!</p>
-		<livewire:service-log-output :servicelog="$log"/>
+	@if($log)
+		{{-- if services_log date is less than datafile modified date, then don't show it --}}
+		@if($log->updated_at < $log->datafile->updated_at)
+			<p>The service is running... The exit code will displayed here when the job is finished.</p>
+		@else
+			<p>Service: {{ $log->name }} (ID: {{ $log->service_id }})</p>
+			<p>Datafile: {{ $log->datafile->name }} (ID: {{ $log->datafile_id }})</p>
+			<p>Created: {{ $log->created_at }} Updated: {{ $log->updated_at }} Execution time: {{ number_format($log->execution_time,2) }} s</p>
+			@if($log->exit_code != 0)
+				<x-alert>Last service job exited with the code {{ $log->exit_code }} ({{ $log->exit_code_text }})
+					<div><b>stdout</b>:<br>
+						{!! nl2br($log->stdout) !!}
+					</div>
+					<div><b>stderr</b>:<br>
+						{!! nl2br($log->stderr) !!}
+					</div>
+				</x-alert>
+			@else
+				<p>Last service job exited with the code {{ $log->exit_code }} ({{ $log->exit_code_text }})</p>
+			@endif
+		@endif
+	@else
+		<p>No service log yet!</p>
+	@endif
 </div>
 @endhasrole
