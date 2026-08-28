c9f8d4a793e7d3cfa310b1d6bdedfaff38e5cff5
Jonnie Stuefer <jonnie@floorspot.org>
2026-08-28 13:23:09 +0200

    Bugfix: displaying livewire service log component for scheduled job no longer throwing exception

--- laravel/app/Livewire/ServiceLogOutput.php
+++ laravel/app/Livewire/ServiceLogOutput.php
@@ -9,18 +9,22 @@ use App\Models\ServiceLog;
 
 class ServiceLogOutput extends Component
 {
-	public ServiceLog $servicelog;
+  public ?ServiceLog $servicelog = null;
 
-	public function render()
-	{
-		//$running = ServiceLog::find($this->serviceId)?->latest_value ??
-		return view('livewire.service-log-output');
-	}
+  public function render()
+  {
+    //$running = ServiceLog::find($this->serviceId)?->latest_value ??
+    return view('livewire.service-log-output');
+  }
 
-	#[Computed]
-	public function formattedValue(): string
-	{
-		// Format to 2 decimal places with comma thousand separators
-		return round($this->servicelog->refresh()->execution_time ?? 0);
-	}
+  #[Computed]
+  public function formattedValue(): string
+  {
+    // Use the null-safe operator (?->) to prevent errors on null
+    if (! $this->servicelog) {
+        return '0';
+    }
+    // Format to 2 decimal places with comma thousand separators
+    return round($this->servicelog->refresh()->execution_time ?? 0);
+  }
 }
